db7579debc6e92d3649c0d7de4bc15d784f0b8c3
Jason Gross <jgross@mit.edu>
2013-12-06 12:51:56 -0500

    Work in progress, has refine1 and refine

--- SAT.v
+++ SAT.v
@@ -1,4 +1,4 @@
-Require Import List Ensembles String.
+Require Import List Ensembles String Setoid RelationClasses.
 Require Import JMeq ProofIrrelevance.
 
 Set Asymmetric Patterns.
@@ -12,57 +12,27 @@ Reserved Notation "x ;; z" (at level 42, right associativity).
 Reserved Notation "'return' x" (at level 40, no associativity).
 
 Delimit Scope comp_scope with comp.
-(*Delimit Scope monad_scope with monad.
 
-(* Copying from http://coq.inria.fr/pylons/contribs/files/lc/v8.4/lc.Monad.html *)
-Class Monad (carrier : Type -> Type) :=
-  { bind : forall X Y,
-             carrier X -> (X -> carrier Y) -> carrier Y;
-    unit : forall X, X -> carrier X;
-    bind_bind :
-      forall X Y Z
-             (f : X -> carrier Y) (g : Y -> carrier Z)
-             (x : carrier X),
-        bind (bind x f) g =
-        bind x (fun u => bind (f u) g);
-    bind_unit : forall X Y (f : X -> carrier Y) (x : X),
-                  bind (unit x) f = f x;
-    unit_bind : forall X (x : carrier X),
-                  bind x (@unit X) = x
-  }.
+Ltac apply_in_hyp lem :=
+  match goal with
+    | [ H : _ |- _ ] => apply lem in H
+  end.
 
-Arguments bind _ _ _%type _%type _ _%monad.
+Ltac apply_in_hyp_no_match lem :=
+  match goal with
+    | [ H : _ |- _ ] => apply lem in H;
+      match type of H with
+        | appcontext[match _ with _ => _ end] => fail 1
+        | _ => idtac
+      end      
+  end.
 
-Notation "x >>= y" := (bind x y) (at level 42, right associativity) : monad_scope.
-Notation "x <- y ; z" := (bind y (fun x => z)) (at level 42, right associativity) : monad_scope.
-Notation "x ;; z" := (bind x (fun _ => z)) (at level 42, right associativity) : monad_scope.
-Notation "'return' x" := (unit x) (at level 40, no associativity) : monad_scope.*)
+Ltac destruct_ex :=
+  repeat match goal with
+           | [ H : ex _ |- _ ] => destruct H; intuition
+         end.
 
-Section funcs.
-  Variable funcs : string -> Type * Type.
-  Inductive Comp : Type -> Type :=
-  | Return : forall A, A -> Comp A
-  | Bind : forall A B, Comp A -> (A -> Comp B) -> Comp B
-  | Call : forall x, fst (funcs x) -> Comp (snd (funcs x))
-  | Pick : forall A, Ensemble A -> Comp A.
-
-  Bind Scope comp_scope with Comp.
-  Global Arguments Bind A%type B%type _%comp _.
-
-  Notation "x >>= y" := (Bind x y) : comp_scope.
-  Notation "x <- y ; z" := (Bind y (fun x => z)) : comp_scope.
-  Notation "x ;; z" := (Bind x (fun _ => z)) : comp_scope.
-  Notation "f [[ x ]]" := (@Call f x) (at level 35) : comp_scope.
-  Notation "{ x  |  P }" := (@Pick _ (fun x => P)) : comp_scope.
-  Notation "{ x : A  |  P }" := (@Pick A (fun x => P)) : comp_scope.
-
-  Definition Or : Comp bool -> Comp bool -> Comp bool
-    := fun c1 c2 =>
-         (b1 <- c1;
-          if b1
-          then Return true
-          else c2)%comp.
-
+Section formulas.
   Inductive formula (vars : Type) :=
   | Atomic : vars -> formula vars
   | And : formula vars -> formula vars -> formula vars
@@ -100,40 +70,86 @@ Section funcs.
          | And x y => And (subst_vars bool_map x) (subst_vars bool_map y)
          | Not x => Not (subst_vars bool_map x)
        end.
+End formulas.
 
-  Definition sat var (dec_eq_vars : forall x y : var, {x = y} + {x <> y})
-             (sat_func : formula var -> Comp bool)
-             (f : formula var)
-  : Comp bool :=
-    (x0 <- { x0 : option var
-           | (x0 = None /\ get_vars f = Empty_set _)
-             \/ (exists x', x0 = Some x' /\ In _ (get_vars f) x') };
-     match x0 with
-       | None => Return (denote_formula (fun _ => false (* do we want to use a [Prop] here? *)) f)
-       | Some x0 =>
-         let bool_map_t v := if dec_eq_vars x0 v then inr true else inl v in
-         let bool_map_f v := if dec_eq_vars x0 v then inr false else inl v in
-         let formula_t := subst_vars bool_map_t f in
-         let formula_f := subst_vars bool_map_f f in
-         Or (sat_func formula_t) (sat_func formula_f)
-     end)%comp.
+Section funcs.
+  Variable funcs : string -> Type * Type.
+  Inductive Comp : Type -> Type :=
+  | Return : forall A, A -> Comp A
+  | Bind : forall A B, Comp A -> (A -> Comp B) -> Comp B
+  | Call : forall x, fst (funcs x) -> Comp (snd (funcs x))
+  | Pick : forall A, Ensemble A -> Comp A.
 
-  Inductive computes_to (denote_funcs : forall name, fst (funcs name) -> Comp (snd (funcs name)))
-  : forall A, Comp A -> A -> Prop :=
-  | ReturnComputes : forall A v, @computes_to denote_funcs A (Return v) v
+  Bind Scope comp_scope with Comp.
+  Global Arguments Bind A%type B%type _%comp _.
+
+  Notation "x >>= y" := (Bind x y) : comp_scope.
+  Notation "x <- y ; z" := (Bind y (fun x => z)) : comp_scope.
+  Notation "x ;; z" := (Bind x (fun _ => z)) : comp_scope.
+  Notation "f [[ x ]]" := (@Call f x) (at level 35) : comp_scope.
+  Notation "{ x  |  P }" := (@Pick _ (fun x => P)) : comp_scope.
+  Notation "{ x : A  |  P }" := (@Pick A (fun x => P)) : comp_scope.
+
+  Definition Or : Comp bool -> Comp bool -> Comp bool
+    := fun c1 c2 =>
+         (b1 <- c1;
+          if b1
+          then Return true
+          else c2)%comp.
+
+  Variable denote_funcs : forall name, fst (funcs name) -> Comp (snd (funcs name)).
+
+  Inductive computes_to
+  : forall A : Type, Comp A -> A -> Prop :=
+  | ReturnComputes : forall A v, @computes_to A (Return v) v
   | BindComputes : forall A B comp_a f comp_a_value comp_b_value,
-                     @computes_to denote_funcs A comp_a comp_a_value
-                     -> @computes_to denote_funcs B (f comp_a_value) comp_b_value
-                     -> @computes_to denote_funcs B (Bind comp_a f) comp_b_value
-  | PickComputes : forall A (P : Ensemble A) v, P v -> @computes_to denote_funcs A (Pick P) v
+                     @computes_to A comp_a comp_a_value
+                     -> @computes_to B (f comp_a_value) comp_b_value
+                     -> @computes_to B (Bind comp_a f) comp_b_value
+  | PickComputes : forall A (P : Ensemble A) v, P v -> @computes_to A (Pick P) v
   | CallComputes : forall name (input : fst (funcs name)) (output_v : snd (funcs name)),
-                     @computes_to denote_funcs _ (denote_funcs name input) output_v
-                     -> @computes_to denote_funcs _ (Call name input) output_v.
+                     @computes_to _ (denote_funcs name input) output_v
+                     -> @computes_to _ (Call name input) output_v.
 
+  Theorem computes_to_inv A (c : Comp A) v
+    : computes_to c v -> match c with
+                           | Return _ x => fun v => v = x
+                           | Bind _ _ x f => fun v => exists comp_a_value,
+                             computes_to x comp_a_value
+                             /\ computes_to (f comp_a_value) v
+                           | Call name input => computes_to (denote_funcs name input)
+                           | Pick _ P => P
+                         end v.
+  Proof.
+    destruct 1; eauto.
+  Qed.
+
+  (** The old program might be non-deterministic, and the new program
+      less so.  This means we want to say that if [new] can compute to
+      [v], then [old] should be able to compute to [v], too. *)
+  Definition refine A (old new : Comp A) := forall v, computes_to new v -> computes_to old v.
+  Definition refine1 A X (old new : X -> Comp A) := forall x, refine (old x) (new x).
+
+  Global Instance refine_refl A : Reflexive (@refine A)
+    := fun _ _ x => x.
+  Global Instance refine_trans A : Transitive (@refine A).
+  Proof.
+    repeat intro.
+    unfold refine in *.
+    eauto.
+  Qed.
+
+  Global Instance refine1_refl A B : Reflexive (@refine1 A B)
+    := fun _ _ _ x => x.
+  Global Instance refine1_trans A B : Transitive (@refine1 A B).
+  Proof.
+    unfold refine1 in *.
+    intro.
+    intuition eauto.
+    eapply transitivity; eauto.
+  Qed.
 
   Section monad.
-    Variable denote_funcs : forall name, fst (funcs name) -> Comp (snd (funcs name)).
-
     Local Ltac t :=
       split;
       intro;
@@ -149,24 +165,263 @@ Section funcs.
                    | eapply BindComputes; (eassumption || (try eassumption; [])) ].
 
     Lemma bind_bind X Y Z (f : X -> Comp Y) (g : Y -> Comp Z) x v
-    : computes_to denote_funcs (Bind (Bind x f) g) v
-      <-> computes_to denote_funcs (Bind x (fun u => Bind (f u) g)) v.
+    : computes_to (Bind (Bind x f) g) v
+      <-> computes_to (Bind x (fun u => Bind (f u) g)) v.
     Proof.
       t.
     Qed.
 
     Lemma bind_unit X Y (f : X -> Comp Y) x v
-    : computes_to denote_funcs (Bind (Return x) f) v
-      <-> computes_to denote_funcs (f x) v.
+    : computes_to (Bind (Return x) f) v
+      <-> computes_to (f x) v.
     Proof.
       t.
     Qed.
 
     Lemma unit_bind X (x : Comp X) v
-    : computes_to denote_funcs (Bind x (@Return X)) v
-      <-> computes_to denote_funcs x v.
+    : computes_to (Bind x (@Return X)) v
+      <-> computes_to x v.
     Proof.
       t.
     Qed.
   End monad.
+End funcs.
+
+Hint Constructors computes_to.
+
+Notation "x >>= y" := (Bind x y) : comp_scope.
+Notation "x <- y ; z" := (Bind y (fun x => z)) : comp_scope.
+Notation "x ;; z" := (Bind x (fun _ => z)) : comp_scope.
+Notation "'call' f 'from' funcs [[ x ]]" := (@Call funcs f x) (at level 35) : comp_scope.
+Notation "{ x  |  P }" := (@Pick _ _ (fun x => P)) : comp_scope.
+Notation "{ x : A  |  P }" := (@Pick _ A (fun x => P)) : comp_scope.
+Notation ret := (Return _).
+
+Add Parametric Relation funcs denote_funcs A : (Comp funcs A) (@refine funcs denote_funcs A)
+  reflexivity proved by (@refine_refl funcs denote_funcs A)
+  transitivity proved by (@refine_trans funcs denote_funcs A)
+    as refine_rel.
+
+Add Parametric Relation funcs denote_funcs A B : (B -> Comp funcs A) (@refine1 funcs denote_funcs A B)
+  reflexivity proved by (@refine1_refl funcs denote_funcs A B)
+  transitivity proved by (@refine1_trans funcs denote_funcs A B)
+    as refine1_rel.
+
+Add Parametric Morphism funcs denote_funcs A B : (@Bind funcs A B)
+  with signature (@refine funcs denote_funcs A) ==> (@refine1 funcs denote_funcs B A) ==> (@refine funcs denote_funcs B)
+    as refine_bind.
+Proof.
+  intros.
+  unfold refine1, refine in *.
+  intros.
+  repeat (repeat apply_in_hyp_no_match computes_to_inv;
+    destruct_ex).
+  eauto.
+Qed.
+
+Section op_funcs.
+  Variable op : nat -> nat -> Prop.
+  Variable on_empty : nat -> Prop.
+  Definition is_op (l : list nat) (v : nat) 
+    := Forall (fun n => op v n) l /\ (List.In v l \/ (l = nil /\ on_empty v)).
+
+  Variable funcs : string -> Type * Type.
+  Variable denote_funcs : forall name, fst (funcs name) -> Comp funcs (snd (funcs name)).
+
+  Definition is_op0 (l : list nat) : Comp funcs nat :=
+    { x : nat
+      | is_op l x }%comp.
+
+  Variable concrete_op : nat -> nat -> nat.
+  Variable concrete_on_empty : nat.
+  Hypothesis concrete_op_returns_arg : forall n m,
+    concrete_op n m = n \/ concrete_op n m = m.
+  Hypothesis concrete_op_preserves_op1 : forall n m,
+    op (concrete_op n m) m.
+  Hypothesis concrete_op_preserves_op2 : forall n m,
+    op (concrete_op n m) n.
+  Hypothesis op_refl : Reflexive op.
+  Hypothesis op_trans : Transitive op.
+
+  Definition is_op1 (l : list nat) : Comp funcs (nat : Type) :=
+    (ret (match l with
+            | nil => concrete_on_empty
+            | x::xs => fold_left concrete_op xs x
+          end))%comp.
+
+  Lemma fold_left_concrete_op_preserves_op l
+    : forall acc,
+      op (fold_left concrete_op l acc) acc.
+  Proof.
+    induction l; simpl; eauto.
+  Qed.
+
+  Hint Resolve fold_left_concrete_op_preserves_op.
+
+  Lemma fold_left_concrete_op_returns_in l
+    : forall acc,
+      acc = fold_left concrete_op l acc
+      \/ List.In (fold_left concrete_op l acc) l.
+  Proof.
+    induction l; simpl; eauto.
+    intro acc.
+    specialize (IHl (concrete_op acc a)).
+    destruct IHl; try solve [ intuition eauto ].
+    destruct (concrete_op_returns_arg acc a); intuition eauto;
+      first [ left; congruence
+        | right; left; congruence ].
+  Qed.
+
+  Hint Resolve fold_left_concrete_op_returns_in.
+
+  (*Lemma op_works l
+    : forall acc,
+      let v := (match l with
+                  | nil => acc
+                  | x::xs => fold_left concrete_op xs x
+                end) in
+        Forall (fun n => op v n) l /\ (l <> nil -> List.In v l).
+  Proof.
+    induction l; simpl;
+      intuition eauto.
+    specialize (IHl (concrete_op acc a)); intuition eauto.
+    destruct (fold_left_concrete_op_returns_in l (concrete_op acc a)); intuition eauto.
+    
+    
+    constructor; eauto.
+    
+    
+    apply concrete_op_preserves_
+/    intuition.
+    con
+    constructor.
+    
+    split; [ | intuition ].
+    inversion 1.
+    revert concrete_on_empty.*)
+
+  Theorem is_op_0_1
+    : refine1 denote_funcs is_op0 is_op1.
+  Proof.
+    intros l v old_hyp.
+    apply computes_to_inv in old_hyp.
+  Admitted.
+End op_funcs.
+
+Section min_max_funcs.
+  Definition is_minimum := is_op le (eq 0).
+  Definition is_maximum := is_op ge (eq 0).
+  Definition is_min_max l (min_max : nat * nat) :=
+    is_minimum l (fst min_max) /\ is_maximum l (snd min_max).
+
+  Variable funcs : string -> Type * Type.
+  Variable denote_funcs : forall name, fst (funcs name) -> Comp funcs (snd (funcs name)).
+
+  Definition is_min_max0 (l : list nat) : Comp funcs (nat * nat) := 
+    { x : _
+      | is_min_max l x }%comp.
+
+  Definition is_minimum0 : list nat -> Comp funcs nat := is_op0 le (eq 0) funcs.
+  Definition is_maximum0 : list nat -> Comp funcs nat := is_op0 ge (eq 0) funcs.
+
+  Definition is_minimum1 : list nat -> Comp funcs nat := is_op1 funcs min 0.
+  Definition is_maximum1 : list nat -> Comp funcs nat := is_op1 funcs max 0.
+
+  Theorem refine_is_minimum : refine1 denote_funcs is_minimum0 is_minimum1.
+  Proof.
+    apply is_op_0_1.
+  Qed.
+
+  Theorem refine_is_maximum : refine1 denote_funcs is_maximum0 is_maximum1.
+  Proof.
+    apply is_op_0_1.
+  Qed.
+
+  Lemma refine_pick_pair A B (PA : A -> Prop) (PB : B -> Prop)
+    : refine denote_funcs
+      { x : A * B | PA (fst x) /\ PB (snd x) }%comp
+      (a <- { a : A | PA a };
+        b <- { b : B | PB b };
+        ret (a, b))%comp.
+  Proof.
+    intros (a, b) H.
+    apply computes_to_inv in H.
+    destruct H as [ av [ H1 H2 ] ].
+    apply computes_to_inv in H1.
+    apply computes_to_inv in H2.
+    destruct H2 as [ bv [ H2 H3 ] ].
+    repeat match goal with
+             | _ => constructor; tauto
+             | [ H : _ |- _ ] => apply computes_to_inv in H
+             | [ H : (_, _) = (_, _) |- _ ] => inversion_clear H
+           end.
+  Qed.
+  
+  Definition is_min_max1 : { f : list nat -> Comp funcs (nat * nat) 
+    | forall l, refine denote_funcs (is_min_max0 l) (f l) }.
+  Proof.
+    eexists.
+    intros.
+    unfold is_min_max0.
+    etransitivity.
+    apply refine_pick_pair.
+    rewrite (@refine_is_minimum l).
+    rewrite refi
+    
+          
+        
+    match goal with
+      | [ 
+    intros (
+    refine (proj1_sig 
+  
+  Definition is_minimum0 (l : list nat) : Comp funcs nat :=
+    is_op0
+      { x : nat
+      | is_op l x }%comp.
+
+  Variable concrete_op : nat -> nat -> nat.
+  Variable concrete_on_empty : nat.
+  Hypothesis concrete_op_returns_arg : forall n m,
+    concrete_op n m = n \/ concrete_op n m = m.
+  Hypothesis concrete_op_preserves_op1 : forall n m,
+    op (concrete_op n m) m.
+  Hypothesis concrete_op_preserves_op2 : forall n m,
+    op (concrete_op n m) n.
+  Hypothesis op_refl : Reflexive op.
+  Hypothesis op_trans : Transitive op.
+
+  Definition is_op1 (l : list nat) : Comp funcs (nat : Type) :=
+    (ret (match l with
+            | nil => concrete_on_empty
+            | x::xs => fold_left concrete_op xs x
+          end))%comp.
+
+  
+    
+
+Section sat_funcs.
+  Variable var : Type.
+  Variable dec_eq : forall x y : var, {x = y} + {x <> y}.
+
+  Definition funcs : string -> Type * Type
+    := (fun s =>
+      if string_dec s "sat"
+      then (formula var, bool : Type)
+      else (Datatypes.Empty_set : Type, unit : Type)).
+
+  Definition sat (f : formula var)
+  : Comp funcs bool :=
+    (x0 <- { x0 : option var
+           | (x0 = None /\ get_vars f = Empty_set _)
+             \/ (exists x', x0 = Some x' /\ In _ (get_vars f) x') };
+     match x0 with
+       | None => ret (denote_formula (fun _ => false (* do we want to use a [Prop] here? *)) f)
+       | Some x0 =>
+         let bool_map_t v := if dec_eq x0 v then inr true else inl v in
+         let bool_map_f v := if dec_eq x0 v then inr false else inl v in
+         let formula_t := subst_vars bool_map_t f in
+         let formula_f := subst_vars bool_map_f f in
+         Or (call "sat" from funcs [[ formula_t ]]) (call "sat" from funcs [[ formula_f ]])
+     end)%comp.
 End funcs.
