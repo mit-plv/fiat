6836d789eb9187e33ca4805c93a302d7f21a6932
Bendy <bendy@cs.utexas.edu>
2014-06-27 00:22:57 -0400

    Cache now includes a [Pick] of an element to replace.

--- examples/CacheADT.v
+++ examples/CacheADT.v
@@ -18,18 +18,24 @@ Section CacheADT.
         "LookupKey"   : rep × Key → rep × (option Value)
   }.
 
-  Definition decides (b : bool) (P : Prop)
-    := if b then P else ~ P.
+  Definition EnsembleInsert  {A} (a : A) (ens : Ensemble A) (a' : A)
+    : Prop := a' = a \/ ens a'.
 
-  Definition EnsembleInsert {A} (a : A) (ens : Ensemble A) (a' : A)
-  : Prop := a' = a \/ ens a'.
+  Definition SubEnsembleInsert {A} (a : A) (ens ens' : Ensemble A) (a' : A)
+  : Prop := ens' a' -> EnsembleInsert a ens a'.
+
+  Definition EnsembleRemove
+             (k : Key)
+             (ens : Ensemble (Key * Value))
+             (k' : Key * Value)
+  : Prop := (fst k' <> k /\ ens k').
 
   Definition EnsembleReplace
              (k : Key * Value)
              (ens : Ensemble (Key * Value))
              (k' : Key * Value)
   : Prop := k' = k \/
-            (fst k' <> fst k -> ens k').
+            (EnsembleRemove (fst k) ens k').
 
   Definition ValidLookup
              (r : Ensemble (Key * Value))
@@ -48,7 +54,8 @@ Section CacheADT.
           ret (fun _ => False),
         meth "AddKey" (r : rep, kv : Key * Value) : bool :=
             { r' | (usedKey r (fst kv) -> snd r' = false) /\
-                   (~ usedKey r (fst kv) -> r' = (EnsembleInsert kv r, true))},
+                   (~ usedKey r (fst kv) -> (SubEnsembleInsert kv r (fst r') kv)
+                                            /\ snd r' = true)},
         meth "UpdateKey" (r : rep, kv : Key * Value) : bool :=
               { r' | (usedKey r (fst kv) -> r' = (EnsembleReplace kv r, true)) /\
                      (~ usedKey r (fst kv) -> snd r' = false)},
@@ -65,28 +72,11 @@ Require Import FMapAVL.
 Module StringIndexedMap := FMapAVL.Make String_as_OT.
 Definition MapStringNat := StringIndexedMap.t nat.
 
-Lemma refine_pick_decides {A}
-      (P : Prop)
-      (Q Q' : Ensemble A)
-: refine {a | (P -> Q a) /\
-              (~ P -> Q' a)}
-         (b <- {b | decides b P};
-          if b then
-            {a | Q a}
-          else
-            {a | Q' a}).
-Proof.
-  unfold refine; intros; apply_in_hyp computes_to_inv;
-  destruct_ex; split_and; inversion_by computes_to_inv.
-  destruct x; simpl in *; inversion_by computes_to_inv;
-  econstructor; intuition.
-Qed.
-
-Section UnboundedStringCacheADT.
+Section BoundedStringCacheADT.
 
   Variable Value : Type.
 
-  Definition UnboundedStringCacheADT_AbsR
+  Definition BoundedStringCacheADT_AbsR
              (or : Ensemble (string * Value))
              (nr : StringIndexedMap.t Value) :=
     forall k,
@@ -100,13 +90,13 @@ Section UnboundedStringCacheADT.
   : forall (or : Ensemble (string * Value))
            (nr : StringIndexedMap.t Value)
            (k : string),
-      UnboundedStringCacheADT_AbsR or nr ->
+      BoundedStringCacheADT_AbsR or nr ->
       decides (if StringIndexedMap.find k nr then
                  true else
                  false)
               (usedKey or k).
   Proof.
-    unfold UnboundedStringCacheADT_AbsR, usedKey; intros;
+    unfold BoundedStringCacheADT_AbsR, usedKey; intros;
     pose proof (H k); intuition.
     find_if_inside; simpl; eauto.
     intuition; destruct_ex.
@@ -115,19 +105,20 @@ Section UnboundedStringCacheADT.
 
   Lemma AbsR_add_EnsembleReplace
   : forall nr kv or v,
-      UnboundedStringCacheADT_AbsR or nr
+      BoundedStringCacheADT_AbsR or nr
       -> StringIndexedMap.find (elt:=Value) (fst kv) nr = Some v
-      -> UnboundedStringCacheADT_AbsR
+      -> BoundedStringCacheADT_AbsR
            (EnsembleReplace kv or)
            (StringIndexedMap.add (fst kv) (snd kv) nr).
   Proof.
-    unfold EnsembleReplace, UnboundedStringCacheADT_AbsR;
+    unfold EnsembleReplace, EnsembleRemove,
+    BoundedStringCacheADT_AbsR;
     intros; intuition.
     destruct (string_dec k (fst kv)); subst.
     left.
     rewrite (StringIndexedMap.find_1 (StringIndexedMap.add_1 nr (snd kv) (refl_equal (fst kv))))
       in *; destruct kv; simpl in *; f_equal; congruence.
-    right; intros.
+    right; split; eauto.
     eapply H.
     eauto using StringIndexedMap.find_1, StringIndexedMap.add_3,
     StringIndexedMap.find_2.
@@ -136,27 +127,27 @@ Section UnboundedStringCacheADT.
     destruct (string_dec k (fst kv)); subst.
     eexists; eauto using StringIndexedMap.find_1, StringIndexedMap.add_1.
     destruct (H k).
-    destruct (H3 _ (H2 n)).
+    destruct (H4 _ H3).
     eauto using StringIndexedMap.find_1, StringIndexedMap.add_2,
     StringIndexedMap.find_2.
   Qed.
 
   Lemma AbsR_add_EnsembleInsert
   : forall nr kv or,
-      UnboundedStringCacheADT_AbsR or nr
+      BoundedStringCacheADT_AbsR or nr
       -> StringIndexedMap.find (elt:=Value) (fst kv) nr = None
-      -> UnboundedStringCacheADT_AbsR
-           (EnsembleInsert kv or)
+      -> BoundedStringCacheADT_AbsR
+           (fun kv' => kv' = kv \/ or kv')
            (StringIndexedMap.add (fst kv) (snd kv) nr).
   Proof.
-    unfold EnsembleInsert, UnboundedStringCacheADT_AbsR;
+    unfold SubEnsembleInsert, BoundedStringCacheADT_AbsR;
     intros; intuition.
     destruct (string_dec k (fst kv)); subst.
     left.
     rewrite (StringIndexedMap.find_1 (StringIndexedMap.add_1 nr (snd kv) (refl_equal (fst kv))))
       in *; destruct kv; simpl in *; f_equal; congruence.
-    right; intros.
-    eapply H.
+    right.
+    pose proof (H k); intuition.
     eauto using StringIndexedMap.find_1, StringIndexedMap.add_3,
     StringIndexedMap.find_2.
     subst; simpl in *.
@@ -169,11 +160,63 @@ Section UnboundedStringCacheADT.
     StringIndexedMap.find_2.
   Qed.
 
-  Definition UnboundedStringCacheADT' :
+  Lemma AbsR_add_EnsembleInsertRemove
+  : forall nr or k kv,
+      BoundedStringCacheADT_AbsR or nr
+      -> StringIndexedMap.find (elt:=Value) (fst kv) nr = None
+      -> BoundedStringCacheADT_AbsR
+           (EnsembleInsert kv (EnsembleRemove k or))
+           (StringIndexedMap.add (fst kv) (snd kv) (StringIndexedMap.remove k nr)).
+  Proof.
+    unfold SubEnsembleInsert, EnsembleRemove,
+    EnsembleInsert, BoundedStringCacheADT_AbsR;
+    intros; intuition.
+    destruct (string_dec k0 (fst kv)); subst.
+    left;
+    rewrite (StringIndexedMap.find_1 (StringIndexedMap.add_1 _ (snd kv) (refl_equal (fst kv))))
+      in H1; destruct kv; simpl in *; f_equal; congruence.
+    right; destruct (string_dec k0 k); subst.
+    elimtype False; eapply StringIndexedMap.remove_1 with (x := k); eauto.
+    eapply StringIndexedMap.find_2 in H1.
+    eapply StringIndexedMap.add_3 in H1; eauto.
+    unfold StringIndexedMap.In, StringIndexedMap.Raw.In0; eauto.
+    intuition.
+    eapply H.
+    eapply StringIndexedMap.find_1.
+    eapply StringIndexedMap.remove_3; eauto.
+    eapply StringIndexedMap.add_3; clear n0; eauto.
+    eapply StringIndexedMap.find_2; eauto.
+    subst; simpl.
+    eauto using StringIndexedMap.find_1, StringIndexedMap.add_1.
+    destruct (string_dec k0 (fst kv)); subst.
+    eauto using StringIndexedMap.find_1, StringIndexedMap.add_1.
+    destruct (H k0).
+    destruct (H4 _ H3).
+    eexists.
+    eapply StringIndexedMap.find_2 in H5.
+    eapply StringIndexedMap.remove_2 in H5.
+    eapply StringIndexedMap.add_2 in H5; eauto.
+    eapply StringIndexedMap.find_1; eauto.
+    simpl in *; congruence.
+  Qed.
+
+  Tactic Notation "refine" "pick" "val" constr(v) :=
+    let T := type of v in
+    rewrite refine_pick_val with
+    (A := T)
+      (a := v).
+
+  Tactic Notation "erefine" "pick" "val" open_constr(v) :=
+    let T := type of v in
+    erewrite refine_pick_val with
+    (A := T)
+      (a := v) by reflexivity.
+
+  Definition BoundedStringCacheADT' :
     Sharpened (@CacheSpec string Value).
   Proof.
     unfold CacheSpec; simpl.
-    hone representation using UnboundedStringCacheADT_AbsR.
+    hone representation using BoundedStringCacheADT_AbsR.
 
     hone constructor "EmptyCache".
     {
@@ -182,7 +225,7 @@ Section UnboundedStringCacheADT.
       (A := StringIndexedMap.t Value)
       (a := StringIndexedMap.empty Value).
       finish honing.
-      unfold UnboundedStringCacheADT_AbsR; intros; intuition.
+      unfold BoundedStringCacheADT_AbsR; intros; intuition.
       eapply (StringIndexedMap.empty_1); eauto.
       eapply (StringIndexedMap.find_2); eauto.
     }
@@ -190,14 +233,12 @@ Section UnboundedStringCacheADT.
     hone method "LookupKey".
     {
       simplify with monad laws.
-      rewrite refine_pick_val with
-      (A := option Value)
-        (a := StringIndexedMap.find n r_n).
+      refine pick val (StringIndexedMap.find n r_n).
       simplify with monad laws; simpl.
       rewrite refine_pick_val by eauto.
       simplify with monad laws.
       finish honing.
-      unfold UnboundedStringCacheADT_AbsR, ValidLookup in *;
+      unfold BoundedStringCacheADT_AbsR, ValidLookup in *;
         intros; pose proof (H0 n); intuition.
     }
 
@@ -223,9 +264,7 @@ Section UnboundedStringCacheADT.
         (* If the Key isn't in the cache, do this. *)
         caseEq (StringIndexedMap.find (elt:=Value) (fst n) r_n);
         try discriminate; simpl.
-        erewrite refine_pick_val with
-        (A := prod (Ensemble (string * Value)) bool)
-        (a := (_, _)) by reflexivity.
+        erefine pick val (_, _).
         simplify with monad laws.
         rewrite refine_pick_val by eauto.
         simplify with monad laws; simpl; reflexivity.
@@ -241,29 +280,58 @@ Section UnboundedStringCacheADT.
       eapply refine_if with
       (b := if StringIndexedMap.find (elt:=Value) (fst n) r_n then true else false).
       {
-        (* If the Key is used, do this. *)
+        (* If the Key is used, do nothing. *)
         caseEq (StringIndexedMap.find (elt:=Value) (fst n) r_n);
         try discriminate; simpl.
-        erewrite refine_pick_val with
-        (A := prod (Ensemble (string * Value)) bool)
-        (a := (_, _)) by reflexivity.
+        erefine pick val (_, _).
         simplify with monad laws.
         rewrite refine_pick_val by eauto.
         simplify with monad laws; simpl; reflexivity.
       }
       {
+        (* If the Key is not used, do this. *)
         caseEq (StringIndexedMap.find (elt:=Value) (fst n) r_n);
         try discriminate; simpl.
-        rewrite refineEquiv_pick_eq; simplify with monad laws; simpl.
-        rewrite refine_pick_val by
-          eauto using AbsR_add_EnsembleInsert.
-        simplify with monad laws.
+        refine pick_pair.
+        setoid_rewrite refineEquiv_pick_eq; simplify with monad laws.
+        (* Check if there is a key worth removing *)
+        etransitivity;
+          [ eapply (refine_Pick_Some (A := StringIndexedMap.t Value * bool)
+                                     (fun kv => StringIndexedMap.find (fst kv) r_n = Some (snd kv)))
+          | ].
+        { (* If so, replace it. *)
+          intros.
+          refine pick val (EnsembleInsert n (EnsembleRemove (fst b) or)).
+          simplify with monad laws; simpl.
+          rewrite refine_pick_val
+            by eauto using AbsR_add_EnsembleInsertRemove.
+          simplify with monad laws.
+          higher_order_1_reflexivity.
+          unfold SubEnsembleInsert, EnsembleInsert, EnsembleRemove; eauto.
+        }
+        {
+          (* If not, do a standard insert. *)
+          refine pick val (EnsembleInsert n or).
+          simplify with monad laws; simpl.
+          rewrite refine_pick_val
+            by eauto using AbsR_add_EnsembleInsert.
+          simplify with monad laws.
+          reflexivity.
+          unfold SubEnsembleInsert, EnsembleInsert, EnsembleRemove; eauto.
+        }
+        (* Here's where we can decide how to pick the element to 
+         replace. Picking None lets the cache grow unboundedly. *)
+        refine pick val (@None (StringIndexedMap.key * Value) ).
+        try simplify with monad laws.
         reflexivity.
+        intuition; try discriminate.
       }
+
     }
 
     finish sharpening.
 
   Defined.
 
-End UnboundedStringCacheADT.
+End BoundedStringCacheADT.
+
