77041af51834b2eddb8e5d1c76e260f3111b60d2
Jason Gross <jgross@mit.edu>
2014-06-29 14:54:37 +0100

    Fix .dir-locals.el for new makefile
    Maybe we should have the Makefile generate it in the future?

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,5 +5,5 @@
 						      (expand-file-name ".")))))))
  (coq-mode . ((eval . (let ((default-directory (locate-dominating-file
 						buffer-file-name ".dir-locals.el")))
-			(make-local-variable 'coq-load-path)
-			(setq coq-load-path `((,(expand-file-name ".") "ADTSynthesis"))))))))
+			(make-local-variable 'coq-prog-args)
+			(setq coq-prog-args `("-emacs" "-R" ,(expand-file-name "src") "ADTSynthesis" "-R" ,(expand-file-name "examples") "ADTExamples")))))))
