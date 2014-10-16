95668e9a67b064cec297ac8343c587b3c7840b46
Jason Gross <jgross@mit.edu>
2014-10-16 14:16:30 -0400

    Add some more well-founded lemmas
    This is so that we won't need funext to prove correctness.

--- src/Common/Wf.v
+++ src/Common/Wf.v
@@ -105,4 +105,60 @@ Section wf.
       apply well_founded_sigT_relation_helper; auto.
     Defined.
   End wf_sig.
+
+  Section wf_projT1.
+    Context A (B : A -> Type) (R : relation A).
+
+    Definition projT1_relation : relation (sigT B)
+      := fun ab a'b' =>
+           R (projT1 ab) (projT1 a'b').
+
+    Definition well_founded_projT1_relation : well_founded R -> well_founded projT1_relation.
+    Proof.
+      intros wf [a b]; hnf in *.
+      induction (wf a) as [a H IH].
+      constructor.
+      intros y r.
+      specialize (IH _ r (projT2 y)).
+      destruct y.
+      exact IH.
+    Defined.
+  End wf_projT1.
 End wf.
+
+(** A variant of [Fix] that has a nice [Fix_eq] for functions which
+    doesn't require [functional_extensionality]. *)
+Section Fix.
+  Context A (B : A -> Type) (C : forall a, B a -> Type) (D : forall a b, C a b -> Type)
+          (R : A -> A -> Prop) (Rwf : well_founded R)
+          (P : forall a b c, D a b c -> Type)
+          (F : forall x : A, (forall y : A, R y x -> forall b c d, P y b c d) -> forall b c d, P x b c d).
+
+  Definition Fix3 a b c d : @P a b c d
+    := @Fix { a : A & { b : B a & { c : C b & D c } } }
+            (fun x y => R (projT1 x) (projT1 y))
+            (well_founded_projT1_relation Rwf)
+            (fun abcd => P (projT2 (projT2 (projT2 abcd))))
+            (fun x f => @F (projT1 x) (fun y r b c d => f (existT _ y (existT _ b (existT _ c d))) r) _ _ _)
+            (existT _ a (existT _ b (existT _ c d))).
+
+  Definition Fix3_eq
+             (F_ext : forall x (f g : forall y, R y x -> forall b c d, @P y b c d),
+                        (forall y (p : R y x) b c d, f y p b c d = g y p b c d)
+                        -> forall b c d, @F x f b c d = @F x g b c d)
+  : forall a b c d, @Fix3 a b c d = @F a (fun y (_ : R y a) b c d => @Fix3 y b c d) b c d.
+  Proof.
+    intros.
+    unfold Fix3; simpl.
+    match goal with
+      | [ |- @Fix ?A ?R ?Rwf ?P ?F ?x = _ ]
+        => refine (@Coq.Init.Wf.Fix_eq A R Rwf P F _ x)
+    end.
+    intros; apply F_ext; intros.
+    match goal with
+      | [ H : forall y p, ?f y p = ?g y p
+                          |- ?f ?y ?p = ?g ?y ?p ]
+        => exact (H y p)
+    end.
+  Defined.
+End Fix.
