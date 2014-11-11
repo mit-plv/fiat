2a40dec4e340110820f83c2be8fe46a5b46f2965
Clément Pit--Claudel <clement.pit-claudel@polytechnique.edu>
2014-11-11 05:41:16 -0500

    Add a utility lemma to evar-ify both scas and adts in one step

--- src/FiatToFacade/Compiler/Cleanup.v
+++ src/FiatToFacade/Compiler/Cleanup.v
@@ -32,7 +32,29 @@ Proof.
   intros; specialize_states; split; assumption.
 Qed.
 
+Lemma compile_add_intermediate_step_with_ret :
+  forall (av : Type) (env : Env av) (knowledge : Prop)
+         (k : StringMap.key) (v : Memory.W)
+         (init_scas inter_scas final_scas init_adts inter_adts final_adts : StringMap.t (Value av)),
+    refine
+      (@Prog av env knowledge init_scas ([k >> SCA av v]::final_scas) init_adts
+             final_adts)
+      (p <- (@Prog av env knowledge
+                   init_scas ([k >> SCA av v]::inter_scas)
+                   init_adts inter_adts);
+       q <- (@Prog av env knowledge
+                   ([k >> SCA av v]::inter_scas) ([k >> SCA av v]::final_scas)
+                   inter_adts final_adts);
+       ret (p;
+            q)%facade)%comp.
+Proof.
+  unfold refine, Prog, ProgOk; intros;
+  inversion_by computes_to_inv; subst;
+  constructor; intros; destruct_pairs.
 
+  split; intros; specialize_states; repeat inversion_facade; specialize_states; intuition.
+  repeat (safe_seq; intros; specialize_states; intuition).
+Qed.
 
 Lemma compile_add_intermediate_adts_with_ret :
   forall av env knowledge k v init_scas final_scas init_adts inter_adts final_adts,
