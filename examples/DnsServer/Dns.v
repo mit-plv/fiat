7551f4458ec03344af808abe8077fe2b6cb8492d
Sorawit Suriyakarn <sorawit@mit.edu>
2015-01-08 15:01:51 -0500

    add dns spec

--- examples/DnsServer/Dns.v
+++ examples/DnsServer/Dns.v
@@ -1,11 +1,14 @@
 Require Import AutoDB BagADT.
 Require Import Vector Ascii.
+
 Definition vector A n := t A (N.to_nat n).
 
 Record label :=
   { length : N;
     word : vector ascii length }.
 Definition name := list label.
+
+(* Name or pointer offset to a name *)
 Inductive pname :=  fullname : name -> pname | pointername : N -> pname.
 
 Record question :=
@@ -14,7 +17,7 @@ Record question :=
     qclass : N }.
 
 Record answer :=
-  { aname : pname;
+  { aname : name (* pname *);
     atype : N;
     aclass : N;
     ttl : N;
@@ -28,7 +31,7 @@ Record packet :=
     ancount : N;
     nscount : N;
     arcount : N;
-    questions : vector question qdcount;
+    questions : question (*vector question qdcount*);
     answers : vector answer ancount;
     authority : vector answer nscount;
     additional : vector answer nscount }.
@@ -49,21 +52,49 @@ Definition DnsSchema :=
                            sCLASS :: N,
                            sTYPE :: N,
                            sDLENGTH :: N,
-                           sDATA :: string >]
+                           sDATA :: string> ]
         enforcing [].
+(*
+     + one CNAME record exists, optionally accompanied by SIG, NXT, and
+       KEY RRs,
+     + one or more records exist, none being CNAME records,
+     + the name exists, but has no associated RRs of any type,
+     + the name does not exist at all.
+*)
 
 Definition DnsCache := TupleDef DnsSchema sCOLLECTIONS.
+
 Definition DnsSig : ADTSig :=
   ADTsignature {
       Constructor "Init" : unit -> rep,
       Method "Process" : rep x packet -> rep x packet
   }.
 
+Variable cprefix : name -> name -> Prop.
+Variable lessthaneq : list DnsCache -> DnsCache -> Prop.
+Variable CNAME : N.
+Variable buildempty : packet.
+Variable addns : packet -> DnsCache -> packet.
+Variable addrr : packet -> DnsCache -> packet.
+Variable rec : packet.
+Variable EnsembleEq : forall A, Ensemble A -> list A -> Prop.
+
+Print UnIndexedEnsembleListEquivalence.
 Definition DnsSpec : ADT DnsSig :=
-  QueryADTRep DnsCacheSchema {
+  QueryADTRep DnsSchema {
     Def Constructor "Init" (_ : unit) : rep := empty,
     query "Process" (p : packet) : packet :=
-      rs <- For (r in sDNSCACHE)
-         Where ((qname (question p)) ?? r!sNAME)
-         Return r
+      rs <- For (r in sCOLLECTIONS)
+         Where (cprefix (qname (questions p)) r!sNAME)
+         Return r;
+      bfrs <- { bfrs | forall r, List.In r bfrs <-> List.In r rs /\ lessthaneq rs r };
+      test <- { test | test = true <-> forall b, List.In b bfrs -> (qname (questions p)) = b!sNAME }; 
+      if test
+      then 
+        test2 <- { test2 | test2 = true <-> exists b, List.In b bfrs -> b!sTYPE = CNAME };
+        if andb test2 (N.eqb (qtype (questions p)) CNAME)
+        then ret (List.fold_left addrr bfrs rec)
+        else ret (List.fold_left addrr bfrs buildempty)
+      else 
+        ret (List.fold_left addns bfrs buildempty)
   }.
