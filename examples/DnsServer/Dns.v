e54e1f6149e3099b85d39193940f4c234022d67a
Sorawit Suriyakarn <sorawit@mit.edu>
2015-01-08 23:34:58 -0500

    more definitions in dns.v

--- examples/DnsServer/Dns.v
+++ examples/DnsServer/Dns.v
@@ -1,11 +1,9 @@
 Require Import AutoDB BagADT.
-Require Import Vector Ascii.
-
-Definition vector A n := t A (N.to_nat n).
+Require Import Vector Ascii Bool Bvector List.
 
 Record label :=
   { length : N;
-    word : vector ascii length }.
+    word : string }.
 Definition name := list label.
 
 (* Name or pointer offset to a name *)
@@ -17,24 +15,24 @@ Record question :=
     qclass : N }.
 
 Record answer :=
-  { aname : name (* pname *);
+  { aname : name; (* `pname` if we allow compression *)
     atype : N;
     aclass : N;
     ttl : N;
     rdlength : N;
-    rdata : vector ascii rdlength }.
-    
+    rdata : list ascii }.
+
 Record packet :=
   { id : N;
-    flags : N;
+    flags : Bvector 16; (* 16 bits of boolean *)
     qdcount : N;
     ancount : N;
     nscount : N;
     arcount : N;
-    questions : question (*vector question qdcount*);
-    answers : vector answer ancount;
-    authority : vector answer nscount;
-    additional : vector answer nscount }.
+    questions : question; (* `list question` in case we can have multiple questions? *)
+    answers : list answer;
+    authority : list answer;
+    additional : list answer }.
 
 Definition sCOLLECTIONS := "Collections".
 Definition sNAME := "Name".
@@ -53,7 +51,7 @@ Definition DnsSchema :=
                            sTYPE :: N,
                            sDLENGTH :: N,
                            sDATA :: string> ]
-        enforcing [].
+        enforcing [ ].
 (*
      + one CNAME record exists, optionally accompanied by SIG, NXT, and
        KEY RRs,
@@ -62,39 +60,53 @@ Definition DnsSchema :=
      + the name does not exist at all.
 *)
 
-Definition DnsCache := TupleDef DnsSchema sCOLLECTIONS.
-
+Definition DnsTuple := TupleDef DnsSchema sCOLLECTIONS.
 Definition DnsSig : ADTSig :=
   ADTsignature {
       Constructor "Init" : unit -> rep,
       Method "Process" : rep x packet -> rep x packet
   }.
 
-Variable cprefix : name -> name -> Prop.
-Variable lessthaneq : list DnsCache -> DnsCache -> Prop.
-Variable CNAME : N.
-Variable buildempty : packet.
-Variable addns : packet -> DnsCache -> packet.
-Variable addrr : packet -> DnsCache -> packet.
+Definition prefix (p s : name) := exists ps, List.app p ps = s.
+
+Definition upperbound (r : DnsTuple) (rs : list DnsTuple) :=
+  forall r', List.In r' rs -> List.length r!sNAME >= List.length r'!sNAME.
+ 
+Definition cname : N := 5 % N.
+
+Lemma zero_lt_sixteen : lt 0 16. omega. Qed.
+Definition buildempty (input : packet) : packet :=
+  {| id := id input;
+     flags := replace_order (flags input) zero_lt_sixteen true; (* set QR bit *)
+     qdcount := qdcount input;
+     ancount := 0;
+     nscount := 0;
+     arcount := 0;
+     questions := questions input; (*list question*)
+     answers := List.nil;
+     authority := List.nil;
+     additional := List.nil |}.
+
+Variable addns : packet -> DnsTuple -> packet.
+Variable addrr : packet -> DnsTuple -> packet.
 Variable rec : packet.
 Variable EnsembleEq : forall A, Ensemble A -> list A -> Prop.
 
-Print UnIndexedEnsembleListEquivalence.
 Definition DnsSpec : ADT DnsSig :=
   QueryADTRep DnsSchema {
     Def Constructor "Init" (_ : unit) : rep := empty,
     query "Process" (p : packet) : packet :=
       rs <- For (r in sCOLLECTIONS)
-         Where (cprefix (qname (questions p)) r!sNAME)
+         Where (prefix r!sNAME (qname (questions p)))
          Return r;
-      bfrs <- { bfrs | forall r, List.In r bfrs <-> List.In r rs /\ lessthaneq rs r };
-      test <- { test | test = true <-> forall b, List.In b bfrs -> (qname (questions p)) = b!sNAME }; 
-      if test
+      bfrs <- { bfrs | forall r, List.In r bfrs <-> List.In r rs /\ upperbound r rs };
+      matched <- { matched | matched = true <-> forall b, List.In b bfrs -> (qname (questions p)) = b!sNAME }; 
+      if matched
       then 
-        test2 <- { test2 | test2 = true <-> exists b, List.In b bfrs -> b!sTYPE = CNAME };
-        if andb test2 (N.eqb (qtype (questions p)) CNAME)
+        is_cname <- { is_cname | is_cname = true <-> exists b, List.In b bfrs -> b!sTYPE = cname };
+        if andb is_cname (negb (N.eqb (qtype (questions p)) cname))
         then ret (List.fold_left addrr bfrs rec)
-        else ret (List.fold_left addrr bfrs buildempty)
+        else ret (List.fold_left addrr bfrs (buildempty p))
       else 
-        ret (List.fold_left addns bfrs buildempty)
+        ret (List.fold_left addns bfrs (buildempty p))
   }.
