3f716298d72731f22db7c63238e351ba248aa209
Jason Gross <jgross@mit.edu>
2015-03-13 19:41:44 -0400

    Improve grammar notations

--- src/Parsers/ContextFreeGrammarNotations.v
+++ src/Parsers/ContextFreeGrammarNotations.v
@@ -1,4 +1,4 @@
-Require Import Coq.Strings.String Coq.Lists.List.
+Require Import Coq.Strings.String Coq.Strings.Ascii Coq.Lists.List.
 Require Import Parsers.ContextFreeGrammar.
 
 Fixpoint production_of_string (s : string) : production Ascii.ascii
@@ -17,6 +17,11 @@ Fixpoint list_to_productions {T} (default : T) (ls : list (string * T)) : string
                                    else list_to_productions default ls' s
      end.
 
+Fixpoint list_to_grammar {T} (default : productions T) (ls : list (string * productions T)) : grammar T
+  := {| Start_symbol := hd ""%string (map fst ls);
+        Lookup := list_to_productions default ls;
+        Valid_nonterminals := map fst ls |}.
+
 Definition item_ascii := item Ascii.ascii.
 Coercion item_of_char (ch : Ascii.ascii) : item_ascii := Terminal ch.
 Coercion item_of_string (nt : string) : item_ascii := NonTerminal _ nt.
@@ -33,12 +38,17 @@ Bind Scope production_scope with production.
 Delimit Scope productions_scope with productions.
 Delimit Scope productions_assignment_scope with prods_assignment.
 Bind Scope productions_scope with productions.
+Delimit Scope grammar_scope with grammar.
+Bind Scope grammar_scope with grammar.
 Notation "n0 ::== r0" := ((n0 : string)%string, (r0 : productions _)%productions) (at level 100) : production_assignment_scope.
 Notation "[[[ x ;; .. ;; y ]]]" :=
   (list_to_productions (nil::nil) (cons x%prod_assignment .. (cons y%prod_assignment nil) .. )) : productions_assignment_scope.
+Notation "[[[ x ;; .. ;; y ]]]" :=
+  (list_to_grammar (nil::nil) (cons x%prod_assignment .. (cons y%prod_assignment nil) .. )) : grammar_scope.
 
 Local Open Scope string_scope.
 Notation "<< x | .. | y >>" :=
   (@cons (production _) (x)%production .. (@cons (production _) (y)%production nil) .. ) : productions_scope.
 
 Notation "$< x $ .. $ y >$" := (item_ascii_cons x .. (item_ascii_cons y nil) .. ) : production_scope.
+Notation "# c" := (c%char) (at level 0, only parsing) : production_scope.
