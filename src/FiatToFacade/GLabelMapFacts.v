ec4ac21b914b128a3023857eb350455f105d1d29
Jason Gross <jgross@mit.edu>
2015-03-18 16:24:43 -0400

    Work around bug 4066 (include in -quick)
    This is https://coq.inria.fr/bugs/show_bug.cgi?id=4066, It should be
    possible to [Include] a module compiled in -quick mode

--- src/FiatToFacade/GLabelMapFacts.v
+++ src/FiatToFacade/GLabelMapFacts.v
@@ -2,7 +2,10 @@ Require Export Fiat.Common.Coq__8_4__8_5__Compat.
 
   Require Import Bedrock.Platform.Cito.GLabelMap.
   Require Import Coq.FSets.FMapFacts.
-  Include (Properties GLabelMap).
-  Include (Facts GLabelMap).
+  Module bug_4066_workaround_1 := (Properties GLabelMap).
+  Include bug_4066_workaround_1.
+  Module bug_4066_workaround_2 := (Facts GLabelMap).
+  Include bug_4066_workaround_2.
   Require Import Bedrock.Platform.Cito.FMapFacts1.
-  Include (WFacts_fun GLabelKey.GLabel_as_OT GLabelMap).
+  Module bug_4066_workaround_3 := (WFacts_fun GLabelKey.GLabel_as_OT GLabelMap).
+  Include bug_4066_workaround_3.
