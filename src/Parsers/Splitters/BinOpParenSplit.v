828e17475d97e1a84bf95121b558b2ac76acfc5e
Jason Gross <jgross@mit.edu>
2015-03-24 20:14:43 -0400

    parsers: WIP - splitter for num, +, () grammar

---
+++ src/Parsers/Splitters/BinOpParenSplit.v
@@ -0,0 +1,347 @@
+(** * Definition of a boolean-returning CFG parser-recognizer *)
+Require Import Coq.Lists.List Coq.Strings.String.
+Require Import Parsers.ContextFreeGrammar.
+Require Import Parsers.BaseTypes Parsers.BooleanBaseTypes.
+Require Import Parsers.Splitters.RDPList.
+Require Import Common.
+Require Import Common.List.Operations.
+
+Require Import Coq.Init.Wf.
+Require Import Coq.Arith.Wf_nat.
+Require Import Common.Wf.
+
+Require Import Parsers.Splitters.Reflective.
+Require Import Parsers.Splitters.FirstChar.
+
+Require Import Parsers.MinimalParse.
+Require Import Parsers.ContextFreeGrammarNotations.
+Require Import Parsers.Grammars.ExpressionNumPlus.
+
+Set Implicit Arguments.
+Local Open Scope string_scope.
+Local Open Scope string_like_scope.
+
+Local Coercion is_true : bool >-> Sortclass.
+Section StringT.
+  Context {CharType} {String : string_like CharType}
+          (is_bin_op : CharType -> bool)
+          (is_open : CharType -> bool) (is_close : CharType -> bool).
+
+  (** We build a table to tell us where to split.
+
+      For each character, we store an [option nat], and keep a
+      transient [list nat].
+
+      For '(', we store where the corresponding ')' is.
+
+      For ')', we store [None].
+
+      For everything else, we store where the next '+' at the current
+      level of parenthetization is.  We take advantage of
+      associativity; once we cross a '(' ... ')' at the current level,
+      we stop caring about where the next '+' is.
+
+      The transient list stores where the corresponding ')' is for
+      higher levels. *)
+
+  Definition list_of_next_bin_ops_closes (s : String) (higher_closes : list nat)
+  : list (option nat) * list nat
+    := Fold
+         String
+         (fun _ => list (option nat) * list nat)%type
+         (nil, higher_closes)
+         (fun ch _ table_higher_closes =>
+            let '(table, higher_closes) := (fst table_higher_closes, map S (snd table_higher_closes)) in
+            let '(cur_mark, new_higher_closes)
+                := (if is_bin_op ch
+                    then (Some 0,
+                          higher_closes)
+                    else if is_close ch
+                         then (None,
+                               0::higher_closes)
+                         else if is_open ch
+                              then ((hd None (map Some higher_closes)),
+                                    tl higher_closes)
+                              else (option_map S (hd None table),
+                                    higher_closes)) in
+            (cur_mark::table, new_higher_closes))
+         s.
+
+  Lemma list_of_next_bin_ops_closes_compute_cons {ch s hc}
+  : list_of_next_bin_ops_closes ([[ ch ]] ++ s) hc
+    = (let table_higher_closes := list_of_next_bin_ops_closes s hc in
+       let '(table, higher_closes) := (fst table_higher_closes, map S (snd table_higher_closes)) in
+       ((if is_bin_op ch
+         then Some 0
+         else if is_close ch
+              then None
+              else if is_open ch
+                   then hd None (map Some higher_closes)
+                   else option_map S (hd None table))
+          ::table,
+        (if is_bin_op ch
+         then higher_closes
+         else if is_close ch
+              then 0::higher_closes
+              else if is_open ch
+                   then tl higher_closes
+                   else higher_closes))).
+  Proof.
+    unfold list_of_next_bin_ops_closes; simpl.
+    rewrite Fold_compute_cons; simpl.
+    destruct (is_bin_op ch), (is_close ch), (is_open ch); simpl;
+    reflexivity.
+  Qed.
+
+  Let String' : Type := String.
+  Let StringT := (StringWithSplitState String (fun s => { ls : list (option nat)
+                                                        | exists hc, ls = fst (list_of_next_bin_ops_closes s hc) })).
+
+  Definition StringT_of_string (s : String') : StringT
+    := {| string_val := s ; state_val := exist _ (fst (list_of_next_bin_ops_closes s nil)) (ex_intro _ nil eq_refl) |}.
+
+  Lemma drop_list_of_next_bin_ops_closes {s n hc}
+  : drop n (fst (list_of_next_bin_ops_closes s hc)) =
+    fst (list_of_next_bin_ops_closes (snd (SplitAt n s)) hc).
+  Proof.
+    revert s n.
+    match goal with
+      | [ |- forall s, @?P s ]
+        => refine (Fold _ P _ _)
+    end.
+    { intros.
+      rewrite SplitAtEmpty; simpl.
+      unfold list_of_next_bin_ops_closes; simpl.
+      rewrite Fold_compute_empty; simpl.
+      destruct n; reflexivity. }
+    { intros ? ? H n.
+      induction n; simpl.
+      { rewrite SplitAt0; reflexivity. }
+      { rewrite SplitAtS; simpl.
+        rewrite <- H.
+        rewrite list_of_next_bin_ops_closes_compute_cons; simpl; reflexivity. } }
+  Qed.
+
+  Definition SplitAtT (n : nat) (s : StringT) : StringT * StringT.
+  Proof.
+    refine (let s' := SplitAt n s in
+            ({| string_val := fst s';
+                state_val := exist _ (take n (proj1_sig (state_val s))) _ |},
+             {| string_val := snd s';
+                state_val := exist _ (drop n (proj1_sig (state_val s))) _ |}));
+    subst s'.
+    { destruct (state_val s) as [ table [ hc H ] ].
+      exists (snd (list_of_next_bin_ops_closes (snd (SplitAt n s)) hc)).
+      subst; simpl.
+      rewrite <- (SplitAt_correct String n s) at 1.
+      pose proof (Min.le_min_r (Length s) n) as H'.
+      rewrite <- SplitAtLength_correct in H'.
+      revert H'.
+      destruct (SplitAt n s) as [s1 s2]; simpl; clear s.
+
+    { destruct (state_val s) as [ table [ hc H ] ].
+      exists hc; simpl.
+      subst.
+      apply drop_list_of_next_bin_ops_closes. }
+    {
+
+    {
+
+
+    SearchAbout list.
+
+  Definition first_char_split
+             (it : item CharType)
+             (its : production CharType)
+             (str : StringT)
+  : list (StringT * StringT).
+  Proof.
+
+
+
+
+End StringT.
+
+
+  Local Coercion StringT_of_string (s : String') : StringT :=
+    {| string_val := s ; state_val := I |}.
+
+  Lemma StringT_eq {x y : StringT} (H : string_val x = string_val y)
+  : x = y.
+  Proof.
+    destruct x as [? [] ], y as [? [] ]; simpl in *; subst; reflexivity.
+  Qed.
+
+  Local Ltac t_equality :=
+    repeat match goal with
+             | _ => intro
+             | _ => reflexivity
+             | [ H : is_true false |- _ ] => hnf in H; clear -H; exfalso; abstract discriminate
+             | [ H : false = true |- _ ] => hnf in H; clear -H; exfalso; abstract discriminate
+             | _ => progress simpl in *
+             | _ => progress subst
+             | _ => progress destruct_head sigT
+             | _ => progress destruct_head prod
+             | _ => progress destruct_head @StringWithSplitState
+             | [ |- _ = _ :> (_ * _)%type ] => apply f_equal2
+             | [ |- _ = _ :> StringT ] => apply StringT_eq
+             | [ |- _ \/ False ] => left
+             | _ => rewrite RightId
+             | _ => rewrite LeftId
+             | [ H : context[string_dec ?str ?x] |- _ ] => atomic x; destruct (string_dec str x)
+             | [ |- context[match ?s with _ => _ end] ] => atomic s; destruct s
+             | [ H : context[match ?s with _ => _ end] |- _ ] => atomic s; destruct s
+             | _ => rewrite substring_correct3'
+             | [ |- context[SplitAt _ ?n (?s1 ++ ?s2)] ]
+               => replace n with (Length s1) by (rewrite Singleton_Length; trivial);
+                 rewrite SplitAt_concat_correct
+             | [ H : minimal_parse_of_item _ _ _ _ _ _ _ _ (Terminal _) |- _ ] => inversion H; clear H
+             | [ H : minimal_parse_of_item _ _ _ _ _ _ _ _ (NonTerminal _) |- _ ] => inversion H; clear H
+             | [ H : minimal_parse_of_production _ _ _ _ _ _ _ _ (_::_) |- _ ] => inversion H; clear H
+             | [ H : minimal_parse_of_production _ _ _ _ _ _ _ _ nil |- _ ] => inversion H; clear H
+             | _ => solve [ eauto ]
+           end.
+
+  Section first_char_splitter.
+    Context (fallback_split
+             : forall (it : item CharType)
+                      (its : production CharType)
+                      (str : StringT),
+                 list (StringT * StringT)).
+
+    Definition first_char_split
+               (it : item CharType)
+               (its : production CharType)
+               (str : StringT)
+    : list (StringT * StringT)
+      := match its with
+           | nil => [(str, (Empty _ : String') : StringT)]
+           | _::_ => match it with
+                       | Terminal _
+                         => [(((fst (SplitAt _ 1 str) : String') : StringT),
+                              ((snd (SplitAt _ 1 str) : String') : StringT))]
+                       | NonTerminal _
+                         => fallback_split it its str
+                     end
+         end.
+
+    Lemma first_char_split_correct_seq {it its} {str : StringT}
+          (f := fun strs : StringT * StringT => fst strs ++ snd strs =s str)
+          (fallback_split_correct_eq : List.Forall f (fallback_split it its str))
+    : List.Forall f (first_char_split it its str).
+    Proof.
+      unfold first_char_split; subst f.
+      repeat match goal with
+               | _ => reflexivity
+               | _ => assumption
+               | _ => progress simpl in *
+               | [ |- context[match ?l with _ => _ end] ]
+                 => atomic l; destruct l
+               | [ |- is_true (_ =s _)%string_like ] => apply bool_eq_correct
+               | _ => progress rewrite ?LeftId, ?RightId
+               | _ => apply SplitAt_correct
+               | [ |- List.Forall _ [] ] => constructor
+               | [ |- List.Forall _ (_::_) ] => constructor
+             end.
+    Qed.
+
+    Context (G : grammar CharType).
+    Context (fallback_split_correct_eq
+             : forall it its (str : StringT),
+                 List.Forall
+                   (fun strs : StringT * StringT => fst strs ++ snd strs =s str)
+                   (fallback_split it its str)).
+
+    Global Instance first_char_premethods : @parser_computational_dataT' _ _ (@rdp_list_data' _ _ G)
+      := { split_string_for_production str0 valid := first_char_split;
+           split_string_for_production_correct str0 valid it its str := first_char_split_correct_seq (fallback_split_correct_eq it its str) }.
+
+    Global Instance first_char_data : @boolean_parser_dataT _ _
+      := { predata := @rdp_list_predata _ G;
+           split_stateT str := True;
+           split_string_for_production := first_char_split;
+           split_string_for_production_correct it its str := first_char_split_correct_seq (fallback_split_correct_eq it its str) }.
+
+    Fixpoint first_char_valid_prod
+             (first_char_valid : string -> bool)
+             (p : production CharType)
+    : bool
+      := match p with
+           | nil => true
+           | (NonTerminal nt)::nil => first_char_valid nt
+           | (Terminal _)::p' => first_char_valid_prod first_char_valid p'
+           | (NonTerminal _)::_::_ => false
+         end.
+
+    Definition first_char_valid : bool
+      := split_valid (G := G) first_char_valid_prod.
+
+    Lemma first_char_split_complete
+          (H : first_char_valid = true)
+    : forall str0 valid str pf nt,
+        is_valid_nonterminal initial_nonterminals_data nt ->
+        ForallT
+          (Forall_tails
+             (fun prod =>
+                match prod with
+                  | [] => True
+                  | it :: its =>
+                    @split_list_completeT _ String G first_char_data str0 valid str pf
+                                          (@first_char_split it its str) it its
+                end)) (Lookup G nt).
+    Proof.
+      apply (split_complete_simple first_char_valid_prod).
+      { intros ? ? ? p.
+        induction p; t_equality. }
+      { t_equality. }
+      { intros.
+        exists (I, I).
+        unfold first_char_split.
+        t_equality. }
+      { exact H. }
+    Qed.
+
+    Global Instance first_char_cdata' H : @boolean_parser_completeness_dataT' _ _ G first_char_data
+      := { remove_nonterminal_1 := rdp_list_remove_nonterminal_1;
+           remove_nonterminal_2 := rdp_list_remove_nonterminal_2;
+           split_string_for_production_complete := @first_char_split_complete H }.
+
+    Global Instance first_char_cdata H : @boolean_parser_correctness_dataT _ _ G
+      := { data := first_char_data;
+           cdata' := first_char_cdata' H }.
+  End first_char_splitter.
+End StringT.
+
+Section on_ab_star.
+  Definition ab_star_linear_split := first_char_split (String := string_stringlike) (fun _ _ _ => nil).
+  Definition ab_star_linear_split_correct_seq {it its str} : List.Forall _ _
+    := @first_char_split_correct_seq _ string_stringlike (fun _ _ _ => nil) it its str (Forall_nil _).
+
+  Global Instance ab_star_premethods : @parser_computational_dataT' _ string_stringlike (@rdp_list_data' _ _ ab_star_grammar)
+    := first_char_premethods (fun _ _ _ => nil) ab_star_grammar (fun _ _ _ => Forall_nil _).
+  Global Instance ab_star_data : @boolean_parser_dataT _ string_stringlike
+    := first_char_data (fun _ _ _ => nil) ab_star_grammar (fun _ _ _ => Forall_nil _).
+
+  Global Instance ab_star_cdata' : @boolean_parser_completeness_dataT' _ _ ab_star_grammar ab_star_data
+    := first_char_cdata' (fun _ _ _ => nil) ab_star_grammar (fun _ _ _ => Forall_nil _) eq_refl.
+
+  Global Instance ab_star_cdata : @boolean_parser_correctness_dataT _ _ ab_star_grammar
+    := first_char_cdata (String := string_stringlike) (fun _ _ _ => nil) ab_star_grammar (fun _ _ _ => Forall_nil _) eq_refl.
+End on_ab_star.
+
+Section on_ab_star'.
+  Definition ab_star'_linear_split := first_char_split (String := string_stringlike) (fun _ _ _ => nil).
+  Definition ab_star'_linear_split_correct_seq {it its str} : List.Forall _ _
+    := @first_char_split_correct_seq _ string_stringlike (fun _ _ _ => nil) it its str (Forall_nil _).
+
+  Global Instance ab_star'_premethods : @parser_computational_dataT' _ string_stringlike (@rdp_list_data' _ _ ab_star_grammar')
+    := first_char_premethods (fun _ _ _ => nil) ab_star_grammar' (fun _ _ _ => Forall_nil _).
+  Global Instance ab_star'_data : @boolean_parser_dataT _ string_stringlike
+    := first_char_data (fun _ _ _ => nil) ab_star_grammar' (fun _ _ _ => Forall_nil _).
+
+  Global Instance ab_star'_cdata' : @boolean_parser_completeness_dataT' _ _ ab_star_grammar' ab_star'_data
+    := first_char_cdata' (fun _ _ _ => nil) ab_star_grammar' (fun _ _ _ => Forall_nil _) eq_refl.
+
+  Global Instance ab_star'_cdata : @boolean_parser_correctness_dataT _ _ ab_star_grammar'
+    := first_char_cdata (String := string_stringlike) (fun _ _ _ => nil) ab_star_grammar' (fun _ _ _ => Forall_nil _) eq_refl.
+End on_ab_star'.
