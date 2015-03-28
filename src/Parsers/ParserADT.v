39934f951d2c5c3064730ef7e0c0bc4f9b1f777c
Jason Gross <jgross@mit.edu>
2015-03-27 21:40:41 -0400

    Clean up ParserADT

--- src/Parsers/ParserADT.v
+++ src/Parsers/ParserADT.v
@@ -1,10 +1,9 @@
 (** * Reference implementation of a splitter and parser based on that splitter *)
 Require Import ADTNotation.BuildADT ADTNotation.BuildADTSig.
 Require Import ADT.ComputationalADT.
-Require Import Parsers.ContextFreeGrammar.
-Require Import Parsers.BooleanRecognizer.
 Require Import ADTSynthesis.Common.List.Operations.
 Require Import ADTRefinement.GeneralRefinements.
+Require Import Parsers.ParserInterface.
 
 Set Implicit Arguments.
 
@@ -30,13 +29,6 @@ Section ReferenceImpl.
       Method "splits" : rep x item CharType * production CharType -> rep x list nat
     }.
 
-  Definition production_is_reachable (p : item CharType * production CharType) : Prop
-    := exists nt prefix,
-         List.In nt (Valid_nonterminals G)
-         /\ List.In
-              (prefix ++ ((fst p)::(snd p)))
-              (Lookup G nt).
-
   (** Reference implementation of a [String] that can be split *)
   Definition string_spec : ADT string_rep := ADTRep String {
     Def Constructor "new"(s : String) : rep :=
@@ -49,13 +41,7 @@ Section ReferenceImpl.
       ret (snd (SplitAt n s), tt),
 
     Def Method "splits"(s : rep, p : item CharType * production CharType) : list nat :=
-      ls <- { ls : list nat
-            | forall n,
-                n <= Length s
-                -> parse_of_item String G (fst (SplitAt n s)) (fst p)
-                -> parse_of_production String G (snd (SplitAt n s)) (snd p)
-                -> production_is_reachable p
-                -> List.In n ls };
+      ls <- { ls : list nat | split_list_is_complete String G s (fst p) (snd p) ls };
       ret (s, ls)
   }.
 
@@ -68,6 +54,19 @@ Section ReferenceImpl.
       (* exact { r : rep of splitter | rep inv holds with s }. *)
     Admitted.
 
-    Check parse_nonterminal split_stateT.
-    Definition has_parse (s : String) : bool
-parse_nonterminal
+    Local Obligation Tactic := admit.
+
+    Program Definition adt_based_splitter : Splitter String G
+      := {| split_rep := _ (* rep of splitter *);
+            split_take n st := _ (* call "take"(st, n) on splitter *);
+            split_drop n st := _ (* call "drop"(st, n) on splitter *);
+            splits_for st it its := _ (* call "splits"(st, (it, its)) on splitter *);
+            split_invariant str st := _ (* rep inv between String and split_rep *);
+            take_respectful str st n H := _ (* proof that "take" preserves the rep inv w.r.t. its spec *);
+            drop_respectful str st n H := _ (* proof that "drop" preserves the rep inv w.r.t. its spec *);
+            splits_for_complete str st it its H := _ (* proof that "splits" obeys its spec, given that the rep inv holds on the input, by H *) |}.
+    (* Next Obligation.
+    Proof.
+    ...
+    Qed. *)
+End parser.
