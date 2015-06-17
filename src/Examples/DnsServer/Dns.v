a583238f1bbeb85c0c54ab3f8ab92c978830d117
katherineye <kye@princeton.edu>
2015-06-17 17:17:07 -0400

    Prove the prefix/bagfind lemma and clear two admits from the proof. TODO: clean up implicits in lemma

--- src/Examples/DnsServer/Dns.v
+++ src/Examples/DnsServer/Dns.v
@@ -34,10 +34,10 @@ Definition DnsSpec : ADT DnsSig :=
       Repeat 1 initializing n with qname (questions p)
                defaulting rec with (ret (buildempty p))
          {{ rs <- For (r in sCOLLECTIONS)      (* Bind a list of all the DNS entries *)
-                  Where (IsPrefix n r!sNAME) (* prefixed with [n] to [rs] *)
+                  Where (IsSuffix n r!sNAME) (* prefixed with [n] to [rs] *)
                   (* prefix: "com.google" is a prefix of "com.google.scholar" *)
                   Return r;
-            If (is_empty rs)        (* Are there any matching records? *)
+            If (negb (is_empty rs))        (* Are there any matching records? *)
             Then                    (* TODO: reverse these if/then cases *)
               bfrs <- [[r in rs | upperbound name_length rs r]]; (* Find the best match (largest prefix) in [rs] *)
               b <- { b | decides b (forall r, List.In r bfrs -> n = r!sNAME) };
@@ -165,6 +165,217 @@ Proof.
   setoid_rewrite negb_involutive; f_equiv.
 Qed.
 
+(* ------------------------ *)
+(* Debugging lemmas for prefix lemma *)
+
+Check BoundedIndex.
+Print BoundedIndex.
+
+(*
+Lemma minimal: 
+    let heading := <sNAME :: name, sTYPE :: RRecordType, 
+                   sCLASS :: RRecordClass, sTTL :: nat, 
+                   sDATA :: string>%Heading : Heading in
+
+    let BStringId0 := ``(sNAME) (* : BoundedIndex [sNAME; sTYPE; sCLASS; sTTL; sDATA] *) in
+    let BStringId1 := ``(sTYPE) (* : BoundedIndex [sNAME; sTYPE; sCLASS; sTTL; sDATA] *) in
+
+    let schma := {|
+          schemaHeading := heading;
+          attrConstraints := None;
+          tupleConstraints := Some
+                                (fun t t' : Tuple =>
+                                   GetAttribute t BStringId0 =
+                                   GetAttribute t' BStringId0 ->
+                                   GetAttribute t BStringId1 <> CNAME) |}
+                 : Schema in
+ 
+    True.
+(* CNAME error *)
+Proof.
+Admitted.
+
+Check GetAttribute.
+Print GetAttribute.
+Check Attributes.
+Print Attributes.
+Locate GetAttribute.
+
+Lemma minimal_annotated: 
+    let heading := <sNAME :: name, sTYPE :: RRecordType, 
+                   sCLASS :: RRecordClass, sTTL :: nat, 
+                   sDATA :: string>%Heading : Heading in
+
+    let BStringId0 := ``(sNAME) : BoundedIndex [sNAME; sTYPE; sCLASS; sTTL; sDATA] in
+    let BStringId1 := ``(sTYPE) : BoundedIndex [sNAME; sTYPE; sCLASS; sTTL; sDATA] in
+    (* annotated, error goes away when schma is commented out *)
+    let schma := {|
+          schemaHeading := heading;
+          attrConstraints := None;
+          tupleConstraints := Some
+                                (fun t t' : Tuple =>
+                                   (* typeclass magic error? *)
+                                   @GetAttribute heading t BStringId0 =
+                                   @GetAttribute heading t' BStringId0 ->
+                                   @GetAttribute heading t BStringId1 <> CNAME) |}
+                 : Schema in
+ 
+    True.
+Proof.
+
+Admitted.
+*)
+
+Ltac inv H := inversion H; clear H; try subst.
+
+(* TODO: move into another file if possible
+TODO: clean up annotations/typeclass magic *)
+Lemma found_names_are_prefixes_of_question : 
+    (* large context here is pasted from the proof *)
+    let StringId2 := "EqualityIndex" : string in
+    let StringId3 := "Name" : string in
+    let BStringId := ``(sCOLLECTIONS) : BoundedIndex [sCOLLECTIONS] in
+    let BStringId2 := ``(StringId3)
+                      : BoundedIndex [sNAME; sTYPE; sCLASS; sTTL; sDATA] in
+
+
+    let heading := <sNAME :: name, sTYPE :: RRecordType, 
+  sCLASS :: RRecordClass, sTTL :: nat, 
+  sDATA :: string>%Heading : Heading in
+
+    (* let SearchTerm := @BuildIndexSearchTerm heading *)
+    (*                     [{| *)
+    (*                         KindIndexKind := StringId2; *)
+    (*                         KindIndexIndex := BStringId2 |}] :  *)
+    (*                     Type in *)
+
+    (* TODO *)
+    let SearchTerm := @BuildIndexSearchTerm heading
+                  (@Datatypes.cons (@KindIndex heading)
+                     (@Build_KindIndex heading StringId2 BStringId2)
+                     (@Datatypes.nil (@KindIndex heading)))
+                  (prod (option (Domain heading BStringId2))
+                     (@Tuple heading -> bool)) : Type in
+    (* TODO *)
+    (* let SearchUpdateTerm := {| *)
+    (*       BagSearchTermType := SearchTerm; *)
+    (*       BagMatchSearchTerm := MatchIndexSearchTerm; *)
+    (*       BagUpdateTermType := Tuple -> Tuple; *)
+    (*       BagApplyUpdateTerm := fun z : Tuple -> Tuple => z |} *)
+    (*                         : SearchUpdateTerms heading in *)
+
+let  SearchUpdateTerm := @Build_SearchUpdateTerms heading SearchTerm
+                        (@MatchIndexSearchTerm heading
+                           (@Datatypes.cons (@KindIndex heading)
+                              (@Build_KindIndex heading StringId2 BStringId2)
+                              (@Datatypes.nil (@KindIndex heading)))
+                           (prod (option (Domain heading BStringId2))
+                              (@Tuple heading -> bool))
+                           (fun
+                              (st : prod (option (Domain heading BStringId2))
+                                      (@Tuple heading -> bool))
+                              (tup : @Tuple heading) =>
+                            andb
+                              match
+                                @fst (option (Domain heading BStringId2))
+                                  (@Tuple heading -> bool) st 
+                                return bool
+                              with
+                              | Some val =>
+                                  match
+                                    @list_eq_dec string string_dec
+                                      (@GetAttribute heading tup BStringId2)
+                                      val return bool
+                                  with
+                                  | left _ => true
+                                  | right _ => false
+                                  end
+                              | None => true
+                              end
+                              (@snd (option (Domain heading BStringId2))
+                                 (@Tuple heading -> bool) st tup)))
+                        (@Tuple heading -> @Tuple heading)
+                        (fun z : @Tuple heading -> @Tuple heading => z)
+                   : SearchUpdateTerms heading in
+
+    let BStringId0 := ``(sNAME) : BoundedIndex [sNAME; sTYPE; sCLASS; sTTL; sDATA] in
+    let BStringId1 := ``(sTYPE) : BoundedIndex [sNAME; sTYPE; sCLASS; sTTL; sDATA] in
+
+    let schma := {|
+          schemaHeading := heading;
+          attrConstraints := None;
+          tupleConstraints := Some
+                                (fun t t' : Tuple =>
+                                   @GetAttribute heading t BStringId0 =
+                                   @GetAttribute heading t' BStringId0 ->
+                                   @GetAttribute heading t BStringId1 <> CNAME) |}
+                 : Schema in
+    let   nschma := relation sCOLLECTIONS has (schma)%NamedSchema : NamedSchema in
+
+  (* TODO *)
+    (* let Index := icons SearchUpdateTerm *)
+    (*                    (inil *)
+    (*                       (fun ns : NamedSchema => *)
+    (*                          SearchUpdateTerms (schemaHeading (relSchema ns)))) *)
+    (*               : ilist *)
+    (*                  (fun ns : NamedSchema => *)
+    (*                     SearchUpdateTerms (schemaHeading (relSchema ns))) *)
+    (*                  [nschma] in *)
+
+  let Index := @icons NamedSchema
+             (fun ns : NamedSchema =>
+              SearchUpdateTerms (schemaHeading (relSchema ns))) nschma
+             (@Datatypes.nil NamedSchema) SearchUpdateTerm
+             (@inil NamedSchema
+                (fun ns : NamedSchema =>
+                 SearchUpdateTerms (schemaHeading (relSchema ns))))
+        : @ilist NamedSchema
+            (fun ns : NamedSchema =>
+             SearchUpdateTerms (schemaHeading (relSchema ns)))
+            (@Datatypes.cons NamedSchema nschma (@Datatypes.nil NamedSchema)) in
+
+  forall (a : IndexedEnsemble * list Tuple) (d : packet) (r_n : IndexedQueryStructure DnsSchema Index),
+
+    (* hypothesis 1 *)
+        (
+    @CallBagMethod DnsSchema Index BStringId BagFind r_n
+                  (None,
+                   fun y : Tuple =>
+                     ?[IsSuffix_dec (qname (questions d)) (@GetAttribute heading y BStringId0)])
+                  ↝ a) ->
+
+    (* hypothesis 2 *)
+    forall (n' : DNSRRecord),
+      List.In n' (snd a) ->
+
+      (* conclusion *)
+      IsPrefix (get_name n') (qname (questions d)).
+Proof.
+  intros.
+  inv H.
+  inv H1.
+  inv H2.
+  unfold BagMatchSearchTerm in H0.
+  simpl in H0.
+  unfold MatchIndexSearchTerm in H0.
+  simpl in H0.
+  
+  unfold IsSuffix_dec in H0.
+  unfold GetAttribute in H0.
+  (* simpl in H0. *)
+
+  apply filter_In in H0.
+ (* Set Printing All. *)
+  inv H0.
+
+(* TODO fix *)
+  destruct ( @IsPrefix_dec string FindSuffixSearchTerms.Astring_eq
+             (@ith_Bounded Attribute string attrName attrType
+                (AttrList heading) n' BStringId0) (qname (questions d))).
+  apply i.
+  inversion H2.
+Qed.
+
 (* -------------------------------------------------------------------------------------- *)
 
 (* TODO: working on this code; please excuse the comments *)
@@ -174,13 +385,98 @@ Print MostlySharpened.
 Theorem DnsManual :
   MostlySharpened DnsSpec.
 Proof.
+
+  (* the two components here (start honing + GenerateIndexesForAll) are manual versions of
+     partial_master_plan' in AutoDB *)
+
+(*
+  unfold DnsSpec.
+
+start sharpening ADT. {
+  hone method "Process". {
+    Check refine_check_one_longest_prefix_CNAME.
+
+ simplify with monad laws.
+    (* implement_Query             (* in AutoDB, implement_Query' has steps *)
+      ltac:(CombineCase5 SuffixIndexUse EqIndexUse)
+             ltac:(CombineCase10 createEarlySuffixTerm createEarlyEqualityTerm)
+                    ltac:(CombineCase7 createLastSuffixTerm createLastEqualityTerm)
+                           ltac:(CombineCase7 SuffixIndexUse_dep EqIndexUse_dep)
+                                  ltac:(CombineCase11 createEarlySuffixTerm_dep createEarlyEqualityTerm_dep)
+                                         ltac:(CombineCase8 createLastSuffixTerm_dep createLastEqualityTerm_dep). *)
+    (* simplify with monad laws. *)
+    (* Find the upperbound of the results. *)
+    etransitivity.
+    apply refine_under_bind; intros.
+    (* rewrite map_app, map_map, app_nil_r, map_id; simpl. *)
+    match goal with
+      |- refine _ (?H _) => let id := fresh in set (id := H) in *
+    end. (* rename ?whatever to H(number) *)
+    setoid_rewrite refine_If_Then_Else_Bind.
+    (* Should honing if branches also be their own tactic? *)
+    etransitivity.
+    apply refine_If_Then_Else.
+    simplify with monad laws.
+    match goal with
+      |- context [ [[r in ?A | upperbound ?f ?l r]] ] =>
+      pose (@refine_find_upperbound _ f A)
+    end.
+    etransitivity.
+    { apply refine_bind; eauto.
+      intro; higher_order_reflexivity. }
+
+    setoid_rewrite (@refine_decides_forall_In' _ _ _ _).
+    simplify with monad laws.
+    etransitivity.
+
+{
+      apply refine_bind;
+      [ apply refine_check_one_longest_prefix_s | intro; higher_order_reflexivity ].
+      intros. clear H. clear H1. admit.
+    }
+    simplify with monad laws.
+    setoid_rewrite refine_if_If.
+    setoid_rewrite refine_If_Then_Else_Bind.
+    etransitivity.
+    apply refine_If_Then_Else.
+    simplify with monad laws.
+    etransitivity.
+    { apply refine_bind;
+      [ eapply refine_check_one_longest_prefix_CNAME | intro; higher_order_reflexivity ].
+
+      (* [for] query in H0 *)
+      inversion H0.
+      (* need a lemma for reasoning about tuples that result from [for] clauses *)
+      (* for ... ~> a <=> a satisfies... *)
+      inversion H2. clear H2.
+      (* sub-lemma: [a] is included in the enumeration of scollections *)
+      Print QueryStructure.
+      (* crossConstr = data struct x constraint *)
+      Print Relation.
+      (* two kinds of constraints, attr and tuple -- want to use tupleconstr *)
+      (* a in scollection -> a in rel -> tuple part of scollections *)
+      (* need a lemma about it; constraints *****TODO***** *)
+      (* use constraints to justify further refinements -- conditional refinements / preconditions -- e.g. the lemma *)
+      (* "no one else can do this as cleanly as we can *)
+
+      (* ----------- this stuff was copied from the end of the file *)
+    
+*)
   (* unfold MostlySharpened. *)
   start honing QueryStructure. (* what does this do? what's the stuff in the context? *)
+  (* drops the proofs for data integrity *)
 
   (* Implement the constraint checks as queries. *)
   (* original: update "AddData" (t : DNSRRecord) : bool :=
       Insert t into sCOLLECTIONS *)
   (* mutates, so need to check constraints on NAME/CNAME *)
+
+  (* TODO: hone this first here instead? *)
+hone method "Process".
+{
+  finish honing.
+}
+
   hone method "AddData".        (* ? *)
   {
     (* AddData has been expanded in method StringId0 *)
@@ -224,9 +520,16 @@ Proof.
   }
   (* higher level of reasoning *)
 
-  GenerateIndexesForAll
+  GenerateIndexesForAll         (* ? in IndexSelection, see GenerateIndexesFor *)
+  (* specifies that you want to use the suffix index structure TODO *)
   ltac:(CombineCase2 matchFindSuffixIndex matchEqIndex)
          ltac:(fun attrlist => make simple indexes using attrlist).
+  (* SearchTerm and SearchUpdateTerm: efficiently do quality test on the name columns *)
+  (* it figures out what data structure to use *)
+  (* BagMatchSearchTerm *)
+  (* implement query as calls to abstract bag find function *)
+  (* then plug in data structures that impl bag find (chooses b/t them?) *)
+  (*  *)
 
   (* hone constructor "Init". *)
   { 
@@ -271,9 +574,13 @@ Proof.
     - finish honing.
     }
 
-    (* hone method "Process". *) {
+  (* hone method "Process". *) {
+    (* this looks different from the Process method in DnsSpec. how was it changed so far?? *)
+    (* maybe if i remove GenerateIndexesForAll and admit the other cases and do this one, it'll look the same? *)
+    (* where are x0 and x1 coming from? *)
+    (* where's ret (buildempty p)? *)
     simplify with monad laws.
-    implement_Query
+    implement_Query             (* in AutoDB, implement_Query' has steps *)
       ltac:(CombineCase5 SuffixIndexUse EqIndexUse)
              ltac:(CombineCase10 createEarlySuffixTerm createEarlyEqualityTerm)
                     ltac:(CombineCase7 createLastSuffixTerm createLastEqualityTerm)
@@ -287,7 +594,7 @@ Proof.
     rewrite map_app, map_map, app_nil_r, map_id; simpl.
     match goal with
       |- refine _ (?H _) => let id := fresh in set (id := H) in *
-    end.
+    end. (* rename ?whatever to H(number) *)
     setoid_rewrite refine_If_Then_Else_Bind.
     (* Should honing if branches also be their own tactic? *)
     etransitivity.
@@ -304,9 +611,11 @@ Proof.
     setoid_rewrite (@refine_decides_forall_In' _ _ _ _).
     simplify with monad laws.
     etransitivity.
-    { apply refine_bind;
-      (* apply refine_check_one_longest_prefix_s. clear H2 H0. (* follows from the semantics of bagfind *) *)
-      [ apply refine_check_one_longest_prefix_s; clear H2 H0;  admit | intro; higher_order_reflexivity ].
+    { 
+      apply refine_bind;
+      [ apply refine_check_one_longest_prefix_s; clear H2 H0;
+        eapply found_names_are_prefixes_of_question; apply H1
+      | intro; higher_order_reflexivity ].
     }
     simplify with monad laws.
     setoid_rewrite refine_if_If.
@@ -317,11 +626,18 @@ Proof.
     etransitivity.
     { apply refine_bind;
       [ eapply refine_check_one_longest_prefix_CNAME; clear H2 H0 | intro; higher_order_reflexivity ].
-      intros. admit.            (* result of data integrity constraints *)
-      (* should prove it earlier? already dropped the proof *)
+      (* result of data integrity constraints *)
+      (*  where (fun t t' => t!sNAME = t'!sNAME -> t!sTYPE <> CNAME) ] *)
+      (* are the hypotheses here the dynamic check? *)
+      Print nth_error.          (* ? *)
+      intros.
+      admit.
+
+      (* should prove it earlier? where? already dropped the proof -- where? *)
       (* drop condition before honing? *)
-      instantiate (1 := qname (questions d)). admit.
-    (* same as 1st admit? *)
+
+      instantiate (1 := qname (questions d)).
+      eapply found_names_are_prefixes_of_question; apply H1.
     }
     simplify with monad laws.
     apply refine_If_Opt_Then_Else_Bind.
@@ -350,7 +666,7 @@ Proof.
     higher_order_reflexivity.
     simpl.
     finish honing.
-  }
+  } (* ends here *)
   
   FullySharpenQueryStructure' DnsSchema Index.
   (* implement_bag_methods needs to be patched for this goal. *)
