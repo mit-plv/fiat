c83c791ae4afaefc05bcbda96911878f1c28c092
katherineye <kye@princeton.edu>
2015-08-28 13:07:33 -0400

    Add documentation for schema

--- src/Examples/DnsServer/DnsSchema_new.v
+++ src/Examples/DnsServer/DnsSchema_new.v
@@ -6,22 +6,35 @@ Require Import
         Fiat.QueryStructure.Automation.AutoDB
         Fiat.Examples.DnsServer.packet_new.
 
-(* RFC 1034: outline                                                                      
-RFC 1035: more details                                                                 
-RFC 2308: negative caching                                                             
+(* The schema, packet structure, and spec are based on the following four RFCs:
+
+RFC 1034: high-level DNS outline
+RFC 1035: implementation details
+RFC 2308: negative caching
 RFC 1536: common implementation errors and fixes *)
 
+(* What subdomain we're on in a question, e.g. 
+new requests get stage None
+when working on a referral, the stage is set to the match count b/t the referral and question
+e.g. question = s.g.com, referral = for g.com, stage = match count = 2
+(excluding root) *)
 Definition Stage := option nat.
+(* for TTL *)
 Definition time := nat.
+(* unique ids for various things *)
 Definition id : Type := nat.
+(* position in SLIST *)
 Definition position := nat.
-Definition server := name.      (* both IP and server name *)
+(* e.g. ["192", "168", "1", "1"] *)
+Definition IP := name.
 
-(* need this because there's no way to encode failure (no questions) in a packet *)
-(* need this because packet is missing RCODE field that encodes success/kinds of failure *)
+(* Type of things that the outside world (the wrapper) can send to us, the recursive caching   server. Need this because there's no way to encode failure (no questions) in a packet.
+  Packet is missing RCODE field that encodes success/kinds of failure (RFC 2308) *)
 Definition FromOutside : Type := (id * packet * option SOA)%type.
 
+(* Type of the things that we (the server) can send to the wrapper, including various error codes and responses. *)
 Inductive ToOutside : Type :=
+(* Errors *)
 | InvalidQuestion : id -> ToOutside
 | InvalidResponse : id -> ToOutside
 | InvalidPacket : id -> packet -> ToOutside
@@ -29,25 +42,28 @@ Inductive ToOutside : Type :=
 | InternalCacheError : id -> packet -> ToOutside
 | NoReferralsLeft : id -> packet -> ToOutside
 | InvalidId : id -> packet -> ToOutside
-| ServerQuestion : id -> name -> packet -> ToOutside (* name = IP of server to send the packet to *)
+(* Responses *)
+(* We were referred to the server with ip IP, and ask it the same question. Request is pending *)
+| ServerQuestion : id -> IP -> packet -> ToOutside
+(* Request is finished *)
 | ClientAnswer : id -> packet -> ToOutside
 | ClientFailure : id -> packet -> SOA -> ToOutside.
 
+(* Type of things we store for a finished request. (Note no referrals) *)
 Inductive ToStore : Type :=
 | Answer : name -> packet -> ToStore
 | Failure : name -> packet -> SOA -> ToStore.
 
-(* The section of a packet that a certain answer (DNSRRecord) is in. *)
+(* The section of a packet that a certain answer (DNSRRecord) is in, used to tag the flattened rows from a packet. Needed because they're all of type answer so there's no other way to distinguish them *)
 Inductive PacketSection : Type :=
 | PAnswer : PacketSection
 | PAuthority : PacketSection
 | PAdditional : PacketSection.
 
+(* String definitions *)
 Definition sREQUESTS := "Requests".
-(* the # prefix it's on, from front? or back? *)
 Definition sSTAGE := "Stage".
 Definition sID := "ID".
-Definition IP := name.
 Definition sIP := "IP".
 Definition sRESULT := "Result".
 Definition sDOMAIN := "Domain".
@@ -57,17 +73,15 @@ Definition sCACHE_REFERRALS := "Cached referrals".
 Definition sCACHE_ANSWERS := "Cached answers".
 Definition sCACHE_FAILURES := "Cached failures".
 Definition sPACKET_SECTION := "Packet section".
-Definition sSERVER := "Server". (* the server in a Question WrapperResponse *)
+Definition sSERVER := "Server".
 Definition sPID := "Packet id".
 Definition sFLAGS := "Packet flags".
-Definition sNAME := "Record name". (* record string redefined here for clarity *)
-   (* this should be the IP (if not a reverse lookup) *)
+Definition sNAME := "Record name".
 Definition sTYPE := "Record type".
 Definition sCLASS := "Record class".
 Definition sTTL := "Record TTL".
 Definition sRDATA := "Record RDATA".
 
-(* SOA record fields *)
 Definition sHOST := "Source host".
 Definition sEMAIL := "Contact email".
 Definition sSERIAL := "Serial number".
@@ -104,6 +118,8 @@ Definition sSLIST_ORDERS := "SLIST orders".
 Definition sTIME_LAST_CALCULATED := "Time the TTL was last calculated".
 (* initialized with the time the record arrives *)
 
+(* ------------------ Schema headings *)
+
 (* The ideal schema would be domain and WrapperResponse, with some way to query the types nested in WrapperResponse. Flattening it here has the same effect. 
 
 One (Domain => WrapperResponse) mapping is flattened into several rows that all have the same packet information, but store one answer (DNSRRecord) each. 
@@ -118,14 +134,14 @@ Invariants: (TODO)
 - All rows for each domain should appear in exactly 1 of the cache relations (question, answer, or failure). We do not store multiple possibilities.
 - All rows for each domain should have the same packet info. *)
 
-(* For referrals: *)
-(* for domain "brl.mil", referral to suffix "mil": 
-go to server "a.isi.edu" with IP 1.0.0.1 (and ask it the same question) -- RFC 1034 6.2.6
-we discard the original question "brl.mil" *)
-(* 6.3.1 *)
+(* For referrals: 
+ for domain "brl.mil", referral to suffix "mil": 
+ go to server "a.isi.edu" with IP 1.0.0.1 (and ask it the same question). 
+ We discard the original question "brl.mil." 
+ See  RFC 1034 6.2.6, 6.3.1 *)
 Definition SLIST_ReferralHeading :=
   (* R- = referral domain's, S- = server domain's *)
-         <sREQID :: nat,
+         <sREQID :: nat,        (* tuple of (reqid, refid) should be unique for each row *)
           sREFERRALID :: nat,
 
           sREFERRALDOMAIN :: name,
@@ -141,10 +157,11 @@ Definition SLIST_ReferralHeading :=
           (* IP in RDATA of additional record *)
           sTIME_LAST_CALCULATED :: nat,
 
-          sMATCHCOUNT :: nat, (* needed? *)
+          sMATCHCOUNT :: nat,
           sQUERYCOUNT :: nat
          >%Heading.
 
+(* Heading for cached referrals. Same as above but without the "live" information (ids, match and query count) and with cache info (TTL and time the TTL was last calculated) *)
 Definition ReferralHeading :=
   (* R- = referral domain's, S- = server domain's *)
          < sREFERRALDOMAIN :: name,
@@ -161,9 +178,9 @@ Definition ReferralHeading :=
           sTIME_LAST_CALCULATED :: nat
          >%Heading.
 
-(* stores an answer (DNSRRecord) *)
+(* Stores a cached answer (DNSRRecord). Might have appeared in the answer, authority, or additional section of a packet. *)
 (* sDOMAIN and sNAME may differ in the case of CNAME, where 
-sDOMAIN is an alias for sNAME. see RFC 1034, 6.2.7 *)
+sDOMAIN is an alias for sNAME. See RFC 1034, 6.2.7 *)
 Definition AnswerHeading :=
          <sDOMAIN :: name,
           sPACKET_SECTION :: PacketSection,
@@ -175,8 +192,8 @@ Definition AnswerHeading :=
           sTIME_LAST_CALCULATED :: nat
          >%Heading.
 
-          (* stores an SOA (Start of Authority) record according to RFC 2308 *)
-          (* the SOA is technically supposed to go in the Authority section but the packet type doesn't include it *)
+(* Stores an SOA (Start of Authority) record for cached failures, according to RFC 2308. The SOA's TTL is used as the length of time to assume a name failure *)
+(* TODO the SOA is technically supposed to go in the Authority section but the packet type doesn't include it *)
 Definition FailureHeading :=
           <sDOMAIN :: name,
            sHOST :: name,
@@ -189,11 +206,11 @@ Definition FailureHeading :=
            sTIME_LAST_CALCULATED :: nat
           >%Heading.
 
-(* q*, pid, and flags are packet info *)
+(* Heading for a pending request. 
+   Q*, pid, and flags are packet info. Need to store packet info so we can filter the results we get by record type and class. *)
 Definition RequestHeading :=
          <sID :: id,  (* unique, ascending *)
           sQNAME :: name,
-          (* the # domains matched of the name -- left to right or right to left? *)
           sSTAGE :: Stage,      
           sQTYPE :: RRecordType,
           sQCLASS :: RRecordClass,
@@ -208,36 +225,37 @@ Definition AnswerRow := @Tuple AnswerHeading.
 Definition FailureRow := @Tuple FailureHeading.
 Definition RequestRow := @Tuple RequestHeading.
 
-(* when we query here, we want a result type
-that later gets combined with the actual packet in Process
-Process gets ALL the rows from one table (or none) *)
-(* so we can return a list of rows from any table *)
+(* Type of things that a cache query can return. 
+Process gets ALL the rows that match; we don't do any filtering. 
+Process deals with re-hierarchizing, choosing the best result, etc. *)
 Inductive CacheResult :=
 | Nope : CacheResult
 (* Nonempty lists *)
 | Ref : list ReferralRow -> CacheResult
 | Ans : list AnswerRow -> CacheResult
+(* A failure stores exactly one result (the SOA) *)
 | Fail : option FailureRow -> CacheResult.
 
+(* Type used in the cache pointer table, which maps names that exist in the cache
+somewhere to the table that they're in.
+We need to cache referrals, but never in relation to a specific name, which is why 
+they're not in this type. (For a particular request with a name, it will always end in Answer or Failure) *)
 Inductive CacheTable :=
-(* we need to cache referrals, but never in relation to a specific name 
-(for that request, it will always end in Answer or Failure) *)
 | CAnswers
 | CFailures.
 
+(* Type for SLIST order. See RFC 1034, page 32 for a more detailed description of the SLIST.
 
-          (* SLIST 
-each referral needs an id (e.g. x1, x2, x3...) (primary key)
+SLIST: a structure which describes the name servers and the
+                zone which the resolver is currently trying to query.
+                This structure keeps track of the resolver's current
+                best guess about which name servers hold the desired
+                information; it is updated when arriving information
+                changes the guess. 
 
-bound amount of work
+Here, an SLIST is a list of current referrals we have, sorted by descending match count (so the first one to be used should have the highest match count). (TODO: should also incorporate query count) We store the referrals in one table and their positions in another table. Each request's SLIST is a list of (refId, position).
 
-col - # times each referral has been queried
-too many times = delete the row
-
-SLIST needs to be cleared after a request is done
-TTL
-another table that stores ordering information *)
-(* for SLIST *)
+The SLIST is deleted after a request finishes (ends in an answer or failure). Individual referrals may be cleared when their TTL runs out. *)
 Record refPosition :=
   { refId : id;
     refPos : nat }.
@@ -247,7 +265,8 @@ Definition DnsRecSchema :=
         [ relation sREQUESTS has
                    schema
                    RequestHeading;
-
+          
+          (* Described above *)
           relation sSLIST_ORDERS has schema
                    < sREQID :: id,
                      sORDER :: list refPosition
@@ -272,8 +291,6 @@ Definition DnsRecSchema :=
                    FailureHeading
         ]
         enforcing [ ]. 
-          (* where (fun t t' => True) ] *)
-        (* can i have an invariant that just works on one tuple?
-         i want to encode that Stage <= length name *)
-        (* use an attribute constraint TODO *)
-(* TODO: other invariants are not encoded *)
+         (* where (fun t t' => True) ] *)
+         (* TODO use an attribute constraint to encode that Stage <= length name *)
+         (* TODO other invariants are not encoded *)
