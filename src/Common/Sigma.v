2cae017be137de4a4466b5475755a04dc0d36492
Jason Gross <jgross@mit.edu>
2015-10-25 13:16:03 -0400

    Add a tactic to sigma

--- src/Common/Sigma.v
+++ src/Common/Sigma.v
@@ -12,3 +12,22 @@ Proof.
     exists (projT1 f a).
     exact (projT2 f a). }
 Defined.
+
+Ltac pull_forall_sigT :=
+  idtac;
+  (lazymatch goal with
+  | [ |- sigT (fun f : forall a : ?A, @?B a => forall a' : ?A, @?C f a') ]
+    => (let H := fresh in
+        pose proof (fun C' => fst (@pull_forall_sigT A B C')) as H;
+        cbv beta in H;
+        let f' := fresh in
+        let H' := fresh in
+        edestruct H as [f' H']; clear H;
+        [
+        | exists f';
+          let a := fresh in
+          intro a; specialize (H' a);
+          pattern a, (f' a);
+          eexact H' ];
+        cbv beta; intro)
+   end).
