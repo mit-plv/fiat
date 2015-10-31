9f7891f3f07de573e1a5ad816ba4214ba5654316
Jason Gross <jgross@mit.edu>
2015-10-31 15:41:17 -0400

    Move production_is_reachable

--- src/Parsers/ContextFreeGrammar/Properties.v
+++ src/Parsers/ContextFreeGrammar/Properties.v
@@ -9,6 +9,16 @@ Set Implicit Arguments.
 
 Local Open Scope list_scope.
 
+(** A production is reachable if it is the tail of a production
+    associated to a valid nonterminal. *)
+Definition production_is_reachable {Char} (G : grammar Char) (p : production Char)
+: Prop
+  := exists nt prefix,
+       List.In nt (Valid_nonterminals G)
+       /\ List.In
+            (prefix ++ p)
+            (Lookup G nt).
+
 Section cfg.
   Context {Char} {HSL : StringLike Char} {HSLP : @StringLikeProperties Char HSL} (G : grammar Char).
 
