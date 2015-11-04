a18a58f8103180466ffdad0a7fcc0e944c3ccc83
Jason Gross <jgross@mit.edu>
2015-11-04 13:19:10 -0500

    Clean up prettifier a bit

--- src/Parsers/Refinement/Testing/format-test-output.py
+++ src/Parsers/Refinement/Testing/format-test-output.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import with_statement
-import fileinput, re
+import fileinput, re, sys
 
 def format_output(lines):
     FILE_REG = re.compile(r'^(reference|coq)\s*ab(1[0\.]*)$')
@@ -13,9 +13,10 @@ def format_output(lines):
     col1 = '# characters'
     col2 = 'hand-written'
     col3 = 'synthesized'
+    col4 = 'ratio'
     table = []
-    table.append('%s | %s | %s' % (col1, col2, col3))
-    table.append('%s---%s---%s' % ('-' * len(col1), '-' * len(col2), '-' * len(col3)))
+    table.append('%s | %s | %s | %s' % (col1, col2, col3, col4))
+    table.append('%s---%s---%s---%s' % tuple('-' * len(i) for i in (col1, col2, col3, col4)))
     for line in lines:
         fmatch = FILE_REG.match(line.strip())
         tmatch = TIME_REG.match(line.strip())
@@ -25,34 +26,41 @@ def format_output(lines):
             if name != cur_file:
                 if cur_reference is not None or cur_synthesized is not None:
                     num_chars = cur_file.replace('.', '').rjust(len(col1))
+                    if cur_reference is not None and cur_synthesized is not None and cur_reference.strip('.0') != '':
+                        ratio = str(float(cur_synthesized) / float(cur_reference) * 100)
+                    else:
+                        ratio = None
                     cur_reference = ((cur_reference + ' s') if cur_reference is not None else 'None').rjust(len(col2))
                     cur_synthesized = ((cur_synthesized + ' s') if cur_synthesized is not None else 'None').rjust(len(col3))
-                    table.append('%s | %s | %s' % (num_chars, cur_reference, cur_synthesized))
+                    ratio = ((ratio + ' % slower') if ratio is not None else 'N/A').rjust(len(col4))
+                    table.append('%s | %s | %s | %s' % (num_chars, cur_reference, cur_synthesized, ratio))
                 cur_file = name
                 cur_reference = None
                 cur_synthesized = None
         elif tmatch:
             print('%s (%s): %s' % (cur_parser.ljust(len('reference')), cur_file.replace('.', '').ljust(10), line.strip()))
+            sys.stdout.flush()
             (total, median, mean, variance, iterations, per_iteration) = tmatch.groups()
+            time_displayed = median
             if per_iteration.rstrip('0') == '1':
                 while per_iteration[-1] == '0':
-                    if mean[0] == '.': mean = '0' + mean
-                    mean_match = re.match(r'^([0-9]*)\.([0-9]*)$', mean)
-                    if not mean_match:
-                        print('error: %s' % mean)
+                    if time_displayed[0] == '.': time_displayed = '0' + time_displayed
+                    time_displayed_match = re.match(r'^([0-9]*)\.([0-9]*)$', time_displayed)
+                    if not time_displayed_match:
+                        print('error: %s' % time_displayed)
                         break
-                    (pre, post) = mean_match.groups()
-                    mean = pre[:-1] + '.' + pre[-1] + post
-                    if mean[-1] == '0':
-                        mean = mean[:-1]
+                    (pre, post) = time_displayed_match.groups()
+                    time_displayed = pre[:-1] + '.' + pre[-1] + post
+                    if time_displayed[-1] == '0':
+                        time_displayed = time_displayed[:-1]
                     per_iteration = per_iteration[:-1]
-                    if mean[0] == '.': mean = '0' + mean
+                    if time_displayed[0] == '.': time_displayed = '0' + time_displayed
             if cur_parser == 'reference':
-                if cur_reference is not None: print('error: %s (%s, %s)' % (cur_parser, cur_reference, mean))
-                cur_reference = mean
+                if cur_reference is not None: print('error: %s (%s, %s)' % (cur_parser, cur_reference, time_displayed))
+                cur_reference = time_displayed
             elif cur_parser == 'coq':
-                if cur_synthesized is not None: print('error: %s (%s, %s)' % (cur_parser, cur_synthesized, mean))
-                cur_synthesized = mean
+                if cur_synthesized is not None: print('error: %s (%s, %s)' % (cur_parser, cur_synthesized, time_displayed))
+                cur_synthesized = time_displayed
             else:
                 print('error: %s' % cur_parser)
     print('\n'.join(table))
