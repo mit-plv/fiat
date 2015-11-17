075301588796b75dae6ba19c58b3061e661a5d8d
Jason Gross <jgross@mit.edu>
2015-11-17 16:18:38 -0500

    Unfold <= for (S _ <= S _)

--- src/Parsers/ParserImplementationOptimized.v
+++ src/Parsers/ParserImplementationOptimized.v
@@ -83,7 +83,7 @@ Section implementation.
   Local Instance pdata' : @boolean_parser_dataT Ascii.ascii splitter
     := parser_data splitter.
 
-  Local Arguments Compare_dec.leb : simpl never.
+  Local Arguments Compare_dec.leb !_ !_.
 
   Definition parser : Parser G stringlike_stringlike.
   Proof.
