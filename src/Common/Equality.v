0bf95e63048fc674c08ad5ddbf1c73bfae7e3123
Jason Gross <jgross@mit.edu>
2015-11-23 19:13:59 -0500

    Add equality lemma

--- src/Common/Equality.v
+++ src/Common/Equality.v
@@ -444,3 +444,6 @@ Definition concat_pV {A x y} (p : x = y :> A) : eq_trans p (eq_sym p) = eq_refl.
 Proof. case p; reflexivity. Defined.
 Definition concat_Vp {A x y} (p : x = y :> A) : eq_trans (eq_sym p) p = eq_refl.
 Proof. case p; reflexivity. Defined.
+Definition transport_pp {A} {P : A -> Type} {x y z} (p : x = y) (q : y = z) (k : P x)
+: eq_rect _ P k _ (eq_trans p q) = eq_rect _ P (eq_rect _ P k _ p) _ q.
+Proof. case q; simpl; reflexivity. Defined.
