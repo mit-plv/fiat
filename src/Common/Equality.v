b537a69388740d2f1bd8e940cc61d82b42160b3e
Jason Gross <jgross@mit.edu>
2015-11-28 15:06:27 -0500

    Add equality lemmas

--- src/Common/Equality.v
+++ src/Common/Equality.v
@@ -5,6 +5,30 @@ Require Import Fiat.Common.
 Set Implicit Arguments.
 Local Close Scope nat_scope.
 
+Definition concat_1p {A x y} (p : x = y :> A) : eq_trans eq_refl p = p.
+Proof. case p; reflexivity. Defined.
+Definition concat_p1 {A x y} (p : x = y :> A) : eq_trans p eq_refl = p.
+Proof. case p; reflexivity. Defined.
+Definition concat_pV {A x y} (p : x = y :> A) : eq_trans p (eq_sym p) = eq_refl.
+Proof. case p; reflexivity. Defined.
+Definition concat_Vp {A x y} (p : x = y :> A) : eq_trans (eq_sym p) p = eq_refl.
+Proof. case p; reflexivity. Defined.
+Definition transport_pp {A} {P : A -> Type} {x y z} (p : x = y) (q : y = z) (k : P x)
+: eq_rect _ P k _ (eq_trans p q) = eq_rect _ P (eq_rect _ P k _ p) _ q.
+Proof. case q; simpl; reflexivity. Defined.
+Lemma transport_const {A P x y} (p : x = y :> A) k
+: eq_rect _ (fun _ : A => P) k _ p = k.
+Proof. case p; reflexivity. Defined.
+Lemma ap_const {A B x y} (b : B) (p : x = y :> A)
+: f_equal (fun _ => b) p = eq_refl.
+Proof. case p; reflexivity. Defined.
+Lemma inv_pp {A x y z} (p : x = y :> A) (q : y = z :> A)
+: eq_sym (eq_trans p q) = eq_trans (eq_sym q) (eq_sym p).
+Proof. case q; case p; reflexivity. Defined.
+Lemma inv_V {A x y} (p : x = y :> A)
+: eq_sym (eq_sym p) = p.
+Proof. case p; reflexivity. Defined.
+
 Section sigT.
   Definition pr1_path {A} {P : A -> Type} {u v : sigT P} (p : u = v)
   : projT1 u = projT1 v
@@ -30,6 +54,11 @@ Section sigT.
              (p : projT1 u = projT1 v) (q : eq_rect _ _ (projT2 u) _ p = projT2 v)
   : u = v
     := path_sigT_uncurried u v (existT _ p q).
+
+  Definition path_sigT_nondep {A B : Type} (u v : @sigT A (fun _ => B))
+             (p : projT1 u = projT1 v) (q : projT2 u = projT2 v)
+  : u = v
+    := @path_sigT _ _ u v p (eq_trans (transport_const _ _) q).
 
   Lemma eq_rect_sigT {A x} {P : A -> Type} (Q : forall a, P a -> Prop) (u : sigT (Q x)) {y} (H : x = y)
   : eq_rect x (fun a => sigT (Q a)) u y H
@@ -435,27 +464,3 @@ Proof.
   induction a; trivial.
   simpl; rewrite IHa, ascii_dec_refl; reflexivity.
 Qed.
-
-Definition concat_1p {A x y} (p : x = y :> A) : eq_trans eq_refl p = p.
-Proof. case p; reflexivity. Defined.
-Definition concat_p1 {A x y} (p : x = y :> A) : eq_trans p eq_refl = p.
-Proof. case p; reflexivity. Defined.
-Definition concat_pV {A x y} (p : x = y :> A) : eq_trans p (eq_sym p) = eq_refl.
-Proof. case p; reflexivity. Defined.
-Definition concat_Vp {A x y} (p : x = y :> A) : eq_trans (eq_sym p) p = eq_refl.
-Proof. case p; reflexivity. Defined.
-Definition transport_pp {A} {P : A -> Type} {x y z} (p : x = y) (q : y = z) (k : P x)
-: eq_rect _ P k _ (eq_trans p q) = eq_rect _ P (eq_rect _ P k _ p) _ q.
-Proof. case q; simpl; reflexivity. Defined.
-Lemma transport_const {A P x y} (p : x = y :> A) k
-: eq_rect _ (fun _ : A => P) k _ p = k.
-Proof. case p; reflexivity. Defined.
-Lemma ap_const {A B x y} (b : B) (p : x = y :> A)
-: f_equal (fun _ => b) p = eq_refl.
-Proof. case p; reflexivity. Defined.
-Lemma inv_pp {A x y z} (p : x = y :> A) (q : y = z :> A)
-: eq_sym (eq_trans p q) = eq_trans (eq_sym q) (eq_sym p).
-Proof. case q; case p; reflexivity. Defined.
-Lemma inv_V {A x y} (p : x = y :> A)
-: eq_sym (eq_sym p) = p.
-Proof. case p; reflexivity. Defined.
