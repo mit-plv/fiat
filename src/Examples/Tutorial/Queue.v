de8356d0d8f2399403ad75cacda2d35c669e9564
Adam Chlipala <adam@chlipala.net>
2016-01-16 15:59:20 -0500

    Factored more Tutorial functionality into library

--- src/Examples/Tutorial/Queue.v
+++ src/Examples/Tutorial/Queue.v
@@ -56,47 +56,81 @@ Section data.
     destruct ls2; simpl; congruence.
   Qed.
 
-  Hint Resolve tl_cons refine_pick_val.
-  Hint Rewrite <- app_nil_end.
+  Hint Resolve absRel_initial absRel_push tl_cons.
 
-  Definition getNext (conc : list data * list data) : data * (list data * list data) :=
-    match fst conc with
-    | nil =>
-      let ls' := rev (snd conc) in (hd dummy ls', (tl ls', nil))
-    | x :: ls' =>
-      (x, (ls', snd conc))
-    end.
-
-  Lemma absRel_pop_rep : forall abs conc newState,
+  Lemma absRel_must_be_nil : forall abs conc,
     absRel abs conc
-    -> ~(fst conc = nil /\ snd conc = nil)
-    -> newState = getNext conc
-    -> absRel (tl abs) (snd newState).
+    -> fst conc = nil
+    -> snd conc = nil
+    -> abs = nil.
   Proof.
-    unfold absRel; intuition; subst; simpl in *.
-    destruct a, b; simpl in *; intuition (congruence || autorewrite with core; eauto).
+    unfold absRel; destruct conc; simpl; intros; subst; reflexivity.
   Qed.
 
-  Lemma absRel_pop_result : forall abs conc newState,
+  Hint Resolve absRel_must_be_nil.
+
+  Lemma eta_abs_fst : forall abs conc,
     absRel abs conc
-    -> ~(fst conc = nil /\ snd conc = nil)
-    -> newState = getNext conc
-    -> hd dummy abs = fst newState.
+    -> fst conc <> nil
+    -> abs = hd dummy abs :: tl abs.
   Proof.
-    unfold absRel; intuition; subst; simpl in *.
-    destruct a, b; simpl in *; intuition (congruence || autorewrite with core; eauto).
+    unfold absRel; destruct abs; simpl; intuition.
+    destruct (fst conc); simpl in *; intuition congruence.
   Qed.
 
-  Lemma absRel_nonempty : forall x ls conc,
-    absRel (x :: ls) conc
-    -> ~(fst conc = nil /\ snd conc = nil).
+  Lemma eta_abs_snd : forall abs conc,
+    absRel abs conc
+    -> snd conc <> nil
+    -> abs = hd dummy abs :: tl abs.
   Proof.
-    unfold absRel; intros.
-    destruct (fst conc), (snd conc); simpl in *; intuition congruence.
+    unfold absRel; destruct abs; simpl; intros.
+    destruct (snd conc); simpl in *; intuition.
+    apply (f_equal (@length _)) in H.
+    repeat rewrite app_length in H; simpl in H.
+    omega.
+    auto.
   Qed.
 
-  Hint Resolve absRel_initial absRel_push absRel_pop_rep absRel_nonempty.
-  Hint Rewrite absRel_pop_result using tauto.
+  Lemma absRel_reversed_rep : forall abs conc r,
+    absRel abs conc
+    -> fst conc = nil
+    -> snd conc <> nil
+    -> r = rev (snd conc)
+    -> absRel (tl abs) (tl r, nil).
+  Proof.
+    unfold absRel; intuition simpl in *; subst.
+    autorewrite with core; auto.
+  Qed.
+
+  Lemma absRel_reversed_data : forall abs conc r,
+    absRel abs conc
+    -> fst conc = nil
+    -> snd conc <> nil
+    -> r = rev (snd conc)
+    -> hd dummy abs = hd dummy r.
+  Proof.
+    unfold absRel; intuition simpl in *; subst; auto.
+  Qed.
+
+  Lemma absRel_fast_rep : forall abs conc,
+    absRel abs conc
+    -> fst conc <> nil
+    -> absRel (tl abs) (tl (fst conc), snd conc).
+  Proof.
+    unfold absRel; intuition simpl in *; subst.
+    destruct (fst conc); simpl in *; tauto.
+  Qed.
+
+  Lemma absRel_fast_data : forall abs conc,
+    absRel abs conc
+    -> fst conc <> nil
+    -> hd dummy abs = hd dummy (fst conc).
+  Proof.
+    unfold absRel; intuition simpl in *; subst; auto.
+    destruct (fst conc); simpl in *; tauto.
+  Qed.
+
+  Hint Resolve absRel_reversed_rep absRel_fast_rep.
 
   (* Now we start deriving an implementation, in a correct-by-construction way. *)
   Theorem implementation : FullySharpened spec.
@@ -112,166 +146,39 @@ Section data.
     pick.
     finish honing.
 
-    Definition testnil A B (ls : list A) (cnil ccons : B) : B :=
-      match ls with
-      | nil => cnil
-      | _ :: _ => ccons
-      end.
+    refine_testnil (fst r_n).
 
-    Lemma refine_testnil : forall A (ls : list A) B (c1 cnil ccons : Comp B),
-      (ls = nil -> refine c1 cnil)
-      -> (ls <> nil -> refine c1 ccons)
-      -> refine c1 (testnil ls cnil ccons).
-    Proof.
-      destruct ls; intuition congruence.
-    Qed.
-
-    Add Parametric Morphism A B
-    : (@testnil A (Comp B))
-        with signature
-        @eq (list A)
-          ==> @refine B
-          ==> @refine B
-          ==> @refine B
-          as refine_testnil_morphism.
-    Proof.
-      destruct y; auto.
-    Qed.
-
-    Lemma refine_testnil_ret : forall A B (ls : list A) (cnil ccons : B),
-      refine (testnil ls (ret cnil) (ret ccons)) (ret (testnil ls cnil ccons)).
-    Proof.
-      destruct ls; reflexivity.
-    Qed.
-
-    etransitivity.
-    apply refine_testnil with (ls := fst r_n); intro.
-    etransitivity.
-    apply refine_testnil with (ls := snd r_n); intro.
-    
-    Lemma absRel_must_be_nil : forall abs conc,
-      absRel abs conc
-      -> fst conc = nil
-      -> snd conc = nil
-      -> abs = nil.
-    Proof.
-      unfold absRel; destruct conc; simpl; intros; subst; reflexivity.
-    Qed.
-
-    Hint Resolve absRel_must_be_nil.
+    refine_testnil (snd r_n).
 
     assert (r_o = nil) by eauto; subst.
-    simplify with monad laws; simpl.
+    monad_simpl.
     pick.
-    simplify with monad laws; simpl.
+    monad_simpl.
     finish honing.
-
-    Lemma refine_let : forall A B (v : A) c1 (c2 : A -> Comp B),
-      (forall x, x = v -> refine c1 (c2 x))
-      -> refine c1 (let x := v in c2 x).
-    Proof.
-      auto.
-    Qed.
 
     apply refine_let with (v := rev (snd r_n)); intros.
 
-    Lemma eta_abs_snd : forall abs conc,
-      absRel abs conc
-      -> snd conc <> nil
-      -> abs = hd dummy abs :: tl abs.
-    Proof.
-      unfold absRel; destruct abs; simpl; intros.
-      destruct (snd conc); simpl in *; intuition.
-      apply (f_equal (@length _)) in H.
-      repeat rewrite app_length in H; simpl in H.
-      omega.
-      auto.
-    Qed.
-
     erewrite (eta_abs_snd (abs := r_o)) by eauto.
-    simplify with monad laws; simpl.
-
-    Lemma absRel_reversed_rep : forall abs conc r,
-      absRel abs conc
-      -> fst conc = nil
-      -> snd conc <> nil
-      -> r = rev (snd conc)
-      -> absRel (tl abs) (tl r, nil).
-    Proof.
-      unfold absRel; intuition simpl in *; subst.
-      autorewrite with core; auto.
-    Qed.
-
-    Lemma absRel_reversed_data : forall abs conc r,
-      absRel abs conc
-      -> fst conc = nil
-      -> snd conc <> nil
-      -> r = rev (snd conc)
-      -> hd dummy abs = hd dummy r.
-    Proof.
-      unfold absRel; intuition simpl in *; subst; auto.
-    Qed.
-
-    Hint Resolve absRel_reversed_rep.
-
+    monad_simpl.
     pick.
-    simplify with monad laws.
+    monad_simpl.
     erewrite absRel_reversed_data by eauto.
     finish honing.
 
     cbv beta.
     finish honing.
 
-    Lemma eta_abs_fst : forall abs conc,
-      absRel abs conc
-      -> fst conc <> nil
-      -> abs = hd dummy abs :: tl abs.
-    Proof.
-      unfold absRel; destruct abs; simpl; intuition.
-      destruct (fst conc); simpl in *; intuition congruence.
-    Qed.
-
     erewrite (eta_abs_fst (abs := r_o)) by eauto.
-    simplify with monad laws; simpl.
-    
-    Lemma absRel_fast_rep : forall abs conc,
-      absRel abs conc
-      -> fst conc <> nil
-      -> absRel (tl abs) (tl (fst conc), snd conc).
-    Proof.
-      unfold absRel; intuition simpl in *; subst.
-      destruct (fst conc); simpl in *; tauto.
-    Qed.
-
-    Lemma absRel_fast_data : forall abs conc,
-      absRel abs conc
-      -> fst conc <> nil
-      -> hd dummy abs = hd dummy (fst conc).
-    Proof.
-      unfold absRel; intuition simpl in *; subst; auto.
-      destruct (fst conc); simpl in *; tauto.
-    Qed.
-
-    Hint Resolve absRel_fast_rep.
-
-    Lemma reversing_matches_abs : forall abs conc,
-      absRel abs conc
-      -> fst conc = nil
-      -> rev (snd conc) = abs.
-    Proof.
-      unfold absRel; intros.
-      rewrite H0 in *; auto.
-    Qed.
-
+    monad_simpl.
     pick.
-    simplify with monad laws.
+    monad_simpl.
     erewrite absRel_fast_data with (abs := r_o) by eauto.
     finish honing.
 
-    repeat rewrite refine_testnil_ret.
+    cleanup.
     finish honing.
 
-    finish_SharpeningADT_WithoutDelegation.
+    finalize.
   Defined.
 
   Definition impl := Eval simpl in projT1 implementation.
--- src/Examples/Tutorial/Tutorial.v
+++ src/Examples/Tutorial/Tutorial.v
@@ -21,3 +21,75 @@ Export Fiat.QueryStructure.Automation.AutoDB
         Fiat.QueryStructure.Automation.SearchTerms.FindPrefixSearchTerms.
 
 Ltac pick := erewrite refine_pick_val by eauto.
+
+Hint Resolve refine_pick_val.
+Hint Rewrite <- app_nil_end.
+
+Definition testnil A B (ls : list A) (cnil ccons : B) : B :=
+  match ls with
+  | nil => cnil
+  | _ :: _ => ccons
+  end.
+
+Lemma refine_testnil : forall A (ls : list A) B (c1 cnil ccons : Comp B),
+  (ls = nil -> refine c1 cnil)
+  -> (ls <> nil -> refine c1 ccons)
+  -> refine c1 (testnil ls cnil ccons).
+Proof.
+  destruct ls; intuition congruence.
+Qed.
+
+Add Parametric Morphism A B
+: (@testnil A (Comp B))
+    with signature
+    @eq (list A)
+      ==> @refine B
+      ==> @refine B
+      ==> @refine B
+      as refine_testnil_morphism.
+Proof.
+  destruct y; auto.
+Qed.
+
+Lemma refine_testnil_ret : forall A B (ls : list A) (cnil ccons : B),
+  refine (testnil ls (ret cnil) (ret ccons)) (ret (testnil ls cnil ccons)).
+Proof.
+  destruct ls; reflexivity.
+Qed.
+
+Ltac refine_testnil ls' := etransitivity; [ apply refine_testnil with (ls := ls'); intro | ].
+
+Definition let_ A B (v : A) (f : A -> B) := let x := v in f v.
+
+Add Parametric Morphism A B
+: (@let_ A (Comp B))
+    with signature
+    @eq A
+      ==> pointwise_relation _ (@refine B)
+      ==> @refine B
+      as refine_let_morphism.
+Proof.
+  unfold pointwise_relation, let_; auto.
+Qed.
+
+Lemma refine_let : forall A B (v : A) c1 (c2 : A -> Comp B),
+  (forall x, x = v -> refine c1 (c2 x))
+  -> refine c1 (let_ v c2).
+Proof.
+  unfold let_; auto.
+Qed.
+
+Ltac refine_let := apply refine_let.
+
+Lemma refine_let_ret : forall A B (v : A) (f : A -> B),
+  let_ v (fun x => ret (f x)) =  ret (let_ v f).
+Proof.
+  reflexivity.
+Qed.
+
+Ltac monad_simpl := autosetoid_rewrite with refine_monad; simpl.
+
+Hint Rewrite refine_let_ret refine_testnil_ret : cleanup.
+
+Ltac cleanup := autorewrite with cleanup.
+Ltac finalize := finish_SharpeningADT_WithoutDelegation.
