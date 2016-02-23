a6e30855da0fd3cddaff830b9c34ef4d354500e2
Jason Gross <jgross@mit.edu>
2016-02-23 00:41:00 -0500

    More WIP on simple parser

--- src/Parsers/SimpleBooleanRecognizerEquality.v
+++ src/Parsers/SimpleBooleanRecognizerEquality.v
@@ -40,17 +40,32 @@ Section eq.
                | _ => progress subst
                | [ |- _ = _ ] => reflexivity
                | _ => rewrite !lem
+               | _ => rewrite !map_map
+               | [ H : ?x <= ?y, H' : ?x <= ?y |- _ ]
+                 => assert (H = H') by apply Le.le_proof_irrelevance;
+                   subst
+               | [ |- context[?E] ]
+                 => not is_var E;
+                   match type of E with
+                     | _ <= _
+                       => generalize E; intro
+                   end
                | [ |- context[match ?x with _ => _ end] ] => destruct x eqn:?
                | _ => progress unfold option_map
+               | [ |- map _ _ = map _ _ ]
+                 => apply (_ : Proper (pointwise_relation _ _ ==> eq ==> eq) (@map _ _));
+                   [ intro | reflexivity ]
+               | _ => progress simpl in *
+               | _ => congruence
              end.
 
     Lemma parse_item'_eq
-                 (str_matches_nonterminal : nonterminal_carrierT -> bool)
-                 (str_matches_nonterminal' : nonterminal_carrierT -> option simple_parse_of)
-                 (str_matches_nonterminal_eq : forall nt,
-                                                 str_matches_nonterminal nt = str_matches_nonterminal' nt)
-                 (offset : nat) (len : nat)
-                 (it : item Char)
+          (str_matches_nonterminal : nonterminal_carrierT -> bool)
+          (str_matches_nonterminal' : nonterminal_carrierT -> option simple_parse_of)
+          (str_matches_nonterminal_eq : forall nt,
+                                          str_matches_nonterminal nt = str_matches_nonterminal' nt)
+          (offset : nat) (len : nat)
+          (it : item Char)
     : BooleanRecognizer.parse_item' str str_matches_nonterminal offset len it = SimpleRecognizer.parse_item' str str_matches_nonterminal' offset len it.
     Proof.
       t str_matches_nonterminal_eq.
@@ -111,180 +126,164 @@ Section eq.
         { simpl; intros.
           pre_t.
           apply fold_left_option_orb_eq; [ reflexivity | ].
-          rewrite map_map; simpl.
-          apply (_ : Proper (pointwise_relation _ _ ==> eq ==> eq) (@map _ _));
-            [ intro | reflexivity ].
+          t I.
           erewrite parse_item'_eq by (intros; eapply parse_nonterminal_eq).
           rewrite IHps; clear IHps.
-          repeat match goal with
-                   | [ H : ?x <= ?y, H' : ?x <= ?y |- _ ]
-                     => assert (H = H') by apply Le.le_proof_irrelevance;
-                       subst
-                   | [ |- context[?E] ]
-                     => not is_var E;
-                       match type of E with
-                         | _ <= _
-                           => generalize E; intro
-                       end
-                 end.
-          repeat match goal with
-                   | _ => reflexivity
-                   | [ |- context[bool_of_option ?x] ]
-                     => destruct x eqn:?; simpl
-                 end. }
+          t I. }
       Qed.
 
-      Definition parse_production'
-                 (offset : nat)
-                 (len : nat)
-                 (pf : len <= len0)
-                 (prod_idx : production_carrierT)
-      : option simple_parse_of_production
-        := parse_production'_for split_string_for_production offset pf prod_idx.
-      End production.
+      Lemma parse_production'_eq
+            (offset : nat)
+            (len : nat)
+            (pf : len <= len0)
+            (prod_idx : production_carrierT)
+      : BooleanRecognizer.parse_production' str parse_nonterminal offset pf prod_idx
+        = SimpleRecognizer.parse_production' str parse_nonterminal' offset pf prod_idx.
+      Proof.
+        pre_t.
+        apply parse_production'_for_eq.
+      Qed.
+    End production.
 
-      Section productions.
-        Context {len0}
+    Section productions.
+      Context {len0}
+              (parse_nonterminal
+               : forall (offset : nat)
+                        (len : nat)
+                        (pf : len <= len0),
+                   nonterminal_carrierT -> bool)
+              (parse_nonterminal'
+               : forall (offset : nat)
+                        (len : nat)
+                        (pf : len <= len0),
+                   nonterminal_carrierT -> option simple_parse_of)
+              (parse_nonterminal_eq
+               : forall offset len pf nt,
+                   parse_nonterminal offset len pf nt
+                   = parse_nonterminal' offset len pf nt).
+
+      Lemma fold_right_option_simple_parse_of_orb_eq b b' ls ls'
+            (Hb : b = bool_of_option b')
+            (Heq : ls = List.map bool_of_option ls')
+      : fold_right orb b ls
+        = fold_right (@SimpleRecognizer.option_simple_parse_of_orb Char) b' ls'.
+      Proof.
+        subst.
+        revert b'.
+        induction ls' as [|?? IHls']; simpl; [ reflexivity | intro ].
+        rewrite IHls'; destruct b', a; simpl; try reflexivity;
+        t I.
+      Qed.
+
+      Lemma parse_productions'_eq
+            (offset : nat)
+            (len : nat)
+            (pf : len <= len0)
+            (prods : list production_carrierT)
+      : BooleanRecognizer.parse_productions' str parse_nonterminal offset pf prods
+        = SimpleRecognizer.parse_productions' str parse_nonterminal' offset pf prods.
+      Proof.
+        pre_t.
+        apply fold_right_option_simple_parse_of_orb_eq; [ reflexivity | ].
+        t I.
+        erewrite parse_production'_eq; [ reflexivity | ].
+        intros; rewrite parse_nonterminal_eq; reflexivity.
+      Qed.
+    End productions.
+
+    Section nonterminals.
+      Section step.
+        Context {len0 valid_len}
                 (parse_nonterminal
-                 : forall (offset : nat)
-                          (len : nat)
-                          (pf : len <= len0),
-                     nonterminal_carrierT -> option simple_parse_of).
+                 : forall (p : nat * nat),
+                     prod_relation lt lt p (len0, valid_len)
+                     -> forall (valid : nonterminals_listT)
+                               (offset : nat) (len : nat),
+                          len <= fst p -> nonterminal_carrierT -> bool)
+                (parse_nonterminal'
+                 : forall (p : nat * nat),
+                     prod_relation lt lt p (len0, valid_len)
+                     -> forall (valid : nonterminals_listT)
+                               (offset : nat) (len : nat),
+                          len <= fst p -> nonterminal_carrierT -> option simple_parse_of)
+                (parse_nonterminal_eq
+                 : forall (p : nat * nat)
+                          (pf : prod_relation lt lt p (len0, valid_len))
+                          (valid : nonterminals_listT)
+                          (offset : nat) (len : nat)
+                          (pf' : len <= fst p)
+                          (nt : nonterminal_carrierT),
+                     parse_nonterminal p pf valid offset len pf' nt
+                     = parse_nonterminal' p pf valid offset len pf' nt).
 
-        Definition option_simple_parse_of_orb
-                   (x : option simple_parse_of_production)
-                   (xs : option simple_parse_of)
+        Lemma parse_nonterminal_step_eq
+              (valid : nonterminals_listT)
+              (offset : nat)
+              (len : nat)
+              (pf : len <= len0)
+              (nt : nonterminal_carrierT)
+        : BooleanRecognizer.parse_nonterminal_step str parse_nonterminal valid offset pf nt
+          = SimpleRecognizer.parse_nonterminal_step str parse_nonterminal' valid offset pf nt.
+        Proof.
+          pre_t.
+          edestruct dec; simpl; edestruct lt_dec; simpl; try reflexivity;
+          (erewrite parse_productions'_eq; [ reflexivity | ]);
+          intros; rewrite <- parse_nonterminal_eq; try reflexivity.
+          repeat (f_equal; []); intros.
+          apply Le.le_proof_irrelevance.
+        Qed.
+      End step.
+
+      Section wf.
+        Lemma parse_nonterminal_or_abort_eq
+        : forall (p : nat * nat)
+                 (valid : nonterminals_listT)
+                 (offset : nat) (len : nat)
+                 (pf : len <= fst p)
+                 (nt : nonterminal_carrierT),
+            BooleanRecognizer.parse_nonterminal_or_abort str p valid offset pf nt
+            = SimpleRecognizer.parse_nonterminal_or_abort str p valid offset pf nt.
+        Proof.
+          intros.
+                 :
+            -> option simple_parse_of
+          := @Fix
+               (nat * nat)
+               _
+               (well_founded_prod_relation lt_wf lt_wf)
+               _
+               (fun sl => @parse_nonterminal_step (fst sl) (snd sl)).
+
+        Definition parse_nonterminal'
+                   (nt : nonterminal_carrierT)
         : option simple_parse_of
-          := match x, xs with
-               | Some x', _ => Some (SimpleParseHead x')
-               | None, Some xs' => Some (SimpleParseTail xs')
-               | None, None => None
-             end.
+          := @parse_nonterminal_or_abort
+               (length str, nonterminals_length initial_nonterminals_data)
+               initial_nonterminals_data
+               0 (length str)
+               (le_n _) nt.
 
-        (** To parse as a given list of [production]s, we must parse as one of the [production]s. *)
-        Definition parse_productions'
-                   (offset : nat)
-                   (len : nat)
-                   (pf : len <= len0)
-                   (prods : list production_carrierT)
+        Definition parse_nonterminal
+                   (nt : String.string)
         : option simple_parse_of
-          := fold_right option_simple_parse_of_orb
-                        None
-                        (map (parse_production' parse_nonterminal offset pf)
-                             prods).
-      End productions.
+          := parse_nonterminal' (of_nonterminal nt).
+      End wf.
+    End nonterminals.
 
+    Definition parse_item
+               (it : item Char)
+    : option simple_parse_of_item
+      := parse_item' parse_nonterminal' 0 (length str) it.
 
-      Section nonterminals.
-        Section step.
-          Context {len0 valid_len}
-                  (parse_nonterminal
-                   : forall (p : nat * nat),
-                       prod_relation lt lt p (len0, valid_len)
-                       -> forall (valid : nonterminals_listT)
-                                 (offset : nat) (len : nat),
-                            len <= fst p -> nonterminal_carrierT -> option simple_parse_of).
+    Definition parse_production
+               (pat : production_carrierT)
+    : option simple_parse_of_production
+      := parse_production' (parse_nonterminal_or_abort (length str, nonterminals_length initial_nonterminals_data) initial_nonterminals_data) 0 (reflexivity _) pat.
 
-          Definition parse_nonterminal_step
-                     (valid : nonterminals_listT)
-                     (offset : nat)
-                     (len : nat)
-                     (pf : len <= len0)
-                     (nt : nonterminal_carrierT)
-          : option simple_parse_of.
-          Proof.
-            refine
-              (option_rect
-                 (fun _ => option simple_parse_of)
-                 (fun parse_nonterminal
-                  => parse_productions'
-                       parse_nonterminal
-                       offset
-                       (len := len)
-                       (sumbool_rect
-                          (fun b => len <= (if b then len else len0))
-                          (fun _ => le_n _)
-                          (fun _ => _)
-                          (lt_dec len len0))
-                       (nonterminal_to_production nt))
-                 None
-                 (sumbool_rect
-                    (fun b => option (forall (offset' : nat) (len' : nat), len' <= (if b then len else len0) -> nonterminal_carrierT -> option simple_parse_of))
-                    (fun _ => (** [str] got smaller, so we reset the valid nonterminals list *)
-                       Some (@parse_nonterminal
-                               (len, nonterminals_length initial_nonterminals_data)
-                               (or_introl _)
-                               initial_nonterminals_data))
-                    (fun _ => (** [str] didn't get smaller, so we cache the fact that we've hit this nonterminal already *)
-                       sumbool_rect
-                         (fun _ => option (forall (offset' : nat) (len' : nat), len' <= len0 -> nonterminal_carrierT -> option simple_parse_of))
-                         (fun is_valid => (** It was valid, so we can remove it *)
-                            Some (@parse_nonterminal
-                                    (len0, pred valid_len)
-                                    (or_intror (conj eq_refl _))
-                                    (remove_nonterminal valid nt)))
-
-                         (fun _ => (** oops, we already saw this nonterminal in the past.  ABORT! *)
-                            None)
-                         (Sumbool.sumbool_of_bool (negb (EqNat.beq_nat valid_len 0) && is_valid_nonterminal valid nt)))
-                    (lt_dec len len0)));
-            first [ assumption
-                  | simpl;
-                    generalize (proj1 (proj1 (Bool.andb_true_iff _ _) is_valid));
-                    clear; intro;
-                    abstract (
-                        destruct valid_len; try apply Lt.lt_n_Sn; [];
-                        exfalso; simpl in *; congruence
-                  ) ].
-          Defined.
-        End step.
-
-        Section wf.
-          (** TODO: add comment explaining signature *)
-          Definition parse_nonterminal_or_abort
-          : forall (p : nat * nat)
-                   (valid : nonterminals_listT)
-                   (offset : nat) (len : nat),
-              len <= fst p
-              -> nonterminal_carrierT
-              -> option simple_parse_of
-            := @Fix
-                 (nat * nat)
-                 _
-                 (well_founded_prod_relation lt_wf lt_wf)
-                 _
-                 (fun sl => @parse_nonterminal_step (fst sl) (snd sl)).
-
-          Definition parse_nonterminal'
-                     (nt : nonterminal_carrierT)
-          : option simple_parse_of
-            := @parse_nonterminal_or_abort
-                 (length str, nonterminals_length initial_nonterminals_data)
-                 initial_nonterminals_data
-                 0 (length str)
-                 (le_n _) nt.
-
-          Definition parse_nonterminal
-                     (nt : String.string)
-          : option simple_parse_of
-            := parse_nonterminal' (of_nonterminal nt).
-        End wf.
-      End nonterminals.
-
-      Definition parse_item
-                 (it : item Char)
-      : option simple_parse_of_item
-        := parse_item' parse_nonterminal' 0 (length str) it.
-
-      Definition parse_production
-                 (pat : production_carrierT)
-      : option simple_parse_of_production
-        := parse_production' (parse_nonterminal_or_abort (length str, nonterminals_length initial_nonterminals_data) initial_nonterminals_data) 0 (reflexivity _) pat.
-
-      Definition parse_productions
-                 (pats : list production_carrierT)
-      : option simple_parse_of
-        := parse_productions' (parse_nonterminal_or_abort (length str, nonterminals_length initial_nonterminals_data) initial_nonterminals_data) 0 (reflexivity _) pats.
-    End parts.
-  End simple.
+    Definition parse_productions
+               (pats : list production_carrierT)
+    : option simple_parse_of
+      := parse_productions' (parse_nonterminal_or_abort (length str, nonterminals_length initial_nonterminals_data) initial_nonterminals_data) 0 (reflexivity _) pats.
+  End parts.
+End simple.
 End recursive_descent_parser.
