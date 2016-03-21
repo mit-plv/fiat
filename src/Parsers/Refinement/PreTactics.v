0e95d3865cd6171c828331627229b8f855d67cfe
Jason Gross <jgross@mit.edu>
2016-03-21 15:35:23 -0400

    Be a bit less agressive about unfolding opt.nat_of_ascii

--- src/Parsers/Refinement/PreTactics.v
+++ src/Parsers/Refinement/PreTactics.v
@@ -147,6 +147,7 @@ Ltac simplify_parser_splitter' :=
           cbv beta iota zeta;
           simpl @Operations.List.uniquize;
           simpl @List.fold_right
+        | progress simpl @opt.nat_of_ascii
         | progress change (orb false) with (fun x : bool => x); cbv beta
         | progress change (orb true) with (fun x : bool => true); cbv beta
         | progress change (andb false) with (fun x : bool => false); cbv beta
