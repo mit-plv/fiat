ed282b133b062c3b20ea8dd33256de88ae1c5628
Sorawit Suriyakarn <thepsint@gmail.com>
2016-04-04 22:42:51 -0400

    prove enum decode_correct

--- src/BinEncoders/Env/BinLib/Enum.v
+++ src/BinEncoders/Env/BinLib/Enum.v
@@ -28,7 +28,17 @@ Section EnumEncoder.
   Theorem Enum_encode_correct :
     forall predicate, encode_decode_correct cache btransformer predicate Enum_encode Enum_decode.
   Proof.
-  Admitted.
+    intros pred env env' xenv xenv' c c' bin ext ext' Peq Ppred Penc Pdec. simpl in *.
+    unfold Enum_encode, Enum_decode in *.
+    destruct (FixInt_decode size cacheAdd (app bin ext) env') as [[? ?] ?] eqn: eq.
+    pose proof (@FixInt_encode_correct size cache cacheAdd (fun x => pred (A_decode x))
+                   env env' xenv c0 (A_encode c) s bin ext l
+                   Peq) as H.
+    simpl in H. rewrite A_encode_decode_correct in H.
+    specialize (H Ppred Penc eq).
+    inversion Pdec; subst; clear Pdec.
+    intuition; subst; eauto.
+  Qed.
 
   Global Instance Enum_decoder predicate
     : decoder cache btransformer predicate Enum_encode :=
