5ccf3b52f0ffb0fdc2674346163c9dc2193ed69b
Sorawit Suriyakarn <thepsint@gmail.com>
2016-04-13 08:38:42 -0400

    wip binencoders automation

--- src/BinEncoders/Env/Automation/Solver.v
+++ src/BinEncoders/Env/Automation/Solver.v
@@ -1,5 +1,15 @@
-Require Import Fiat.BinEncoders.Env.BinLib.FixInt
-               Fiat.BinEncoders.Env.Common.Sig.
+Require Import Fiat.BinEncoders.Env.Common.Sig
+               Fiat.BinEncoders.Env.Common.Compose.
+
+
+Require Import Fiat.BinEncoders.Env.BinLib.Core
+               Fiat.BinEncoders.Env.BinLib.FixInt
+               Fiat.BinEncoders.Env.BinLib.Char
+               Fiat.BinEncoders.Env.BinLib.Bool
+               Fiat.BinEncoders.Env.BinLib.Enum
+               Fiat.BinEncoders.Env.Lib.FixList
+               Fiat.BinEncoders.Env.Lib.IList
+               Fiat.BinEncoders.Env.Lib.SteppingCacheList.
 
 Ltac enum_part eq_dec :=
   simpl;
@@ -45,3 +55,25 @@ Ltac solve_done :=
   cbv beta; intros; repeat match goal with
                            | [ H : (_, _) = (_, _) |- _ ] => inversion H; subst; clear H
                            end; intuition; subst; eauto.
+
+Ltac solve_predicate :=
+  unfold SteppingList_predicate, IList_predicate, FixList_predicate;
+  intuition eauto; instantiate (1:=fun _ => True); solve_predicate.
+
+Ltac eauto_typeclass :=
+  match goal with
+  | |- context [ Bool_encode ] => eapply Bool_decoder
+  | |- context [ Char_encode ] => eapply Char_decoder
+  | |- context [ FixInt_encode ] => eapply FixInt_decoder
+  | |- context [ FixList_encode _  ] => eapply FixList_decoder
+  | |- context [ IList_encode _ ] => eapply IList_decoder
+  | |- context [ SteppingList_encode _ ] => eapply SteppingListCache_decoder
+  end; eauto.
+
+Ltac solve_decoder :=
+  match goal with
+  | |- _ => solve [ solve_done ]
+  | |- _ => solve [ eauto_typeclass; solve_decoder ]
+  | |- _ => solve [ eapply Enum_decoder; solve_enum ]
+  | |- _ => eapply compose_decoder; [ solve_decoder | solve_predicate | intro; solve_decoder ]
+  end.
