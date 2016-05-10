0ed6ababa96eb163612ae5cdb1d7623ec37449c9
Bendy <bendy@csail.mit.edu>
2016-05-10 12:18:15 -0400

    BinEncoder building block for Enumerated types.

--- src/BinEncoders/Env/Lib2/Enum.v
+++ src/BinEncoders/Env/Lib2/Enum.v
@@ -20,50 +20,99 @@ Section Enum.
   Context {ta : t A (S len)}.
   Variable (tb : t (word sz) (S len)).
 
+  Inductive NoDupVector {A}
+    : forall n, Vector.t A n -> Prop :=
+    NoDupVector_nil : NoDupVector (Vector.nil _)
+  | NoDupVector_cons : forall (x : A) {n} (l : Vector.t A n),
+      ~ Vector.In x l -> NoDupVector l -> NoDupVector (Vector.cons _ x _ l).
+
+  Lemma NoDupVector_invert {A'}
+    : forall n (l : Vector.t A' n),
+      NoDupVector l
+      -> match l with
+         | Vector.nil => True
+         | Vector.cons a _ l' =>
+           ~ Vector.In a l' /\ NoDupVector l'
+         end.
+  Proof.
+    clear; induction 1; eauto.
+  Qed.
+
   Definition encode_enum (i : BoundedIndex ta) (ce : CacheEncode) : B * CacheEncode :=
     let fin := i.(indexb).(ibound)
     in  encode_word (nth tb fin) ce.
 
-  Definition word_indexed (n : nat) (w : word sz) (t : t (word sz) (S n)) : Fin.t (S n).
-    pattern n, t.
-    eapply rectS.
-    intro. exact Fin.F1.
-    intros. destruct (weqb w a).
-    - exact Fin.F1.
-    - exact (Fin.FS H).
-  Defined.
+  Fixpoint word_indexed {n : nat}
+           (w : word sz)
+           (t : t (word sz) n) : option (Fin.t n)
+    := match t in Vector.t _ n return option (Fin.t n) with
+       | nil => None
+       | cons w' _ t' =>
+         if (weqb w w') then Some (Fin.F1) else
+           match word_indexed w t' with
+           | Some f => Some (Fin.FS f)
+           | None => None
+                end
+       end.
 
-  Definition decode_enum (b : B) (cd : CacheDecode) : BoundedIndex ta * B * CacheDecode.
-    destruct (decode_word (sz:=sz) b cd) as [[w b'] cd'].
-    refine ({| bindex := _;
-               indexb := {| ibound := word_indexed w tb;
-                            boundi := _ |} |}, b', cd'); eauto.
-  Defined.
+  Definition decode_enum (b : B)
+             (cd : CacheDecode)
+    : BoundedIndex ta * B * CacheDecode :=
+    match decode_word (sz:=sz) b cd with
+    | (w, b', cd') =>
+      ({| bindex := _;
+          indexb := {| ibound := match word_indexed w tb with
+                                 | Some i => i
+                                 | None => Fin.F1 end;
+                       boundi := eq_refl _ |} |}, b', cd')
+    end.
 
   Lemma word_indexed_correct :
-    forall n (t : t (word sz) (S n)) (i : Fin.t (S n)),
-      word_indexed (nth t i) t = i.
-  Proof. (*
+    forall n (i : Fin.t n) (t : t (word sz) n),
+      NoDupVector t
+      -> match word_indexed (nth t i) t with
+      | Some w' => i = w'
+      | None => False
+      end.
+  Proof.
     clear.
-    intros n t i. revert t. pattern n, i. eapply Fin.rectS.
-    - intros. pattern n0, t. eapply caseS. simpl. intros. destruct t0. eauto.
-      admit.
-    - intros. generalize dependent n0. intro. set (nx := S n0). pattern nx. clearbody nx.
-
-      remember (S n0) as nx. pattern (S n0), t. *)
-  Admitted.
+    induction i.
+    - intro; pattern n, t; apply Vector.caseS; simpl; intros.
+      rewrite (proj2 (weqb_true_iff h h)); eauto.
+    - intro; generalize i (IHi (Vector.tl t)); clear.
+      pattern n, t; apply Vector.caseS; simpl.
+      intros h n0 t0 i; case_eq (word_indexed (nth t0 i) t0); intros;
+        apply NoDupVector_invert in H1; intuition subst.
+      case_eq (weqb (nth t0 t1) h); intros; eauto.
+      apply weqb_true_iff in H0; subst.
+      destruct H2; generalize t0 H; clear; induction t1.
+      + intro; pattern n, t0; apply Vector.caseS; simpl; intros; econstructor.
+      + intro; revert t1 IHt1; pattern n, t0; apply Vector.caseS;
+          simpl; intros.
+        case_eq (weqb (nth t t1) h); intros; eauto.
+        * apply weqb_true_iff in H0; subst; econstructor.
+        * rewrite H0 in H.
+          econstructor 2; apply IHt1.         
+          destruct (word_indexed (nth t t1) t); try discriminate.
+          f_equal; apply Fin.FS_inj; congruence.
+  Qed.
 
   Theorem Enum_decode_correct :
-    encode_decode_correct cache transformer (fun _ => True) encode_enum decode_enum.
+    NoDupVector tb
+    -> encode_decode_correct cache transformer (fun _ => True) encode_enum decode_enum.
   Proof.
     unfold encode_decode_correct, encode_enum, decode_enum.
-    intros env env' xenv xenv' data data' bin' ext ext' Eeq PPred Penc Pdec.
+    intros ? env env' xenv xenv' data data' bin' ext ext' Eeq PPred Penc Pdec.
     destruct (decode_word (transform bin' ext) env') as [[? ?] ?] eqn: ?.
     pose proof (Word_decode_correct _ _ _ _ _ _ _ _ _ Eeq I Penc Heqp).
     inversion Pdec; subst; clear Pdec.
-    destruct H as [? [? ?]].
+    destruct H0 as [? [? ?]].
     repeat split; eauto.
-    rewrite <- H0. rewrite word_indexed_correct.
+    rewrite <- H1.
+    apply (word_indexed_correct (ibound (indexb data))) in H.
+    destruct (word_indexed (nth tb (ibound (indexb data))) tb); intros.
+    rewrite <- H; 
     destruct data. destruct indexb. simpl. rewrite <- boundi. reflexivity.
+    intuition.
   Qed.
 End Enum.
