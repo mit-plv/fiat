a5ce4995bd0f0e06332421ebd0044fb3b322e09f
Jason Gross <jgross@mit.edu>
2016-05-26 23:37:39 -0400

    Add option facts

--- src/Common/OptionFacts.v
+++ src/Common/OptionFacts.v
@@ -1,5 +1,43 @@
 Require Import Coq.Classes.Morphisms.
 
+Set Implicit Arguments.
+
+Scheme Minimality for option Sort Type.
+Scheme Minimality for option Sort Set.
+Scheme Minimality for option Sort Prop.
+
+Lemma fold_option_rect {A B} (S : forall a : A, B (Some a)) (N : B None) (v : option A)
+  : match v return B v with
+    | Some x => S x
+    | None => N
+    end = option_rect B S N v.
+Proof.
+  reflexivity.
+Qed.
+Lemma fold_option_rect_nd {A B} (S : A -> B) (N : B) (v : option A)
+  : match v return B with
+    | Some x => S x
+    | None => N
+    end = option_rect (fun _ => B) S N v.
+Proof.
+  reflexivity.
+Qed.
+
+Global Instance option_rect_Proper_nd_forall {A B}
+  : Proper (pointwise_relation _ eq ==> eq ==> forall_relation (fun _ => eq))
+           (@option_rect A (fun _ => B)).
+Proof.
+  intros ?????? [?|]; subst; simpl; eauto.
+Qed.
+Lemma fold_option_rect_nodep {A B} (S : A -> B) (N : B) (v : option A)
+  : match v return B with
+    | Some x => S x
+    | None => N
+    end = option_rect_nodep S N v.
+Proof.
+  reflexivity.
+Qed.
+
 Lemma option_map_None A B (f : A -> B) x
 : option_map f x = None <-> x = None.
 Proof.
