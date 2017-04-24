be859423a372784542fbe89f554cfdb111586738
John Wiegley <johnw@newartisans.com>
2017-04-23 22:12:45 -0700

    Add a missing argument

--- src/ADTRefinement.v
+++ src/ADTRefinement.v
@@ -3,5 +3,5 @@ Require Export Fiat.ADTRefinement.Core
         Fiat.ADTRefinement.GeneralRefinements
         Fiat.ADTRefinement.Refinements.
 
-Arguments refineMethod / _ _ _ _ _ _ _.
+Arguments refineMethod / _ _ _ _ _ _ _ _.
 Arguments refineConstructor / _ _ _ _ _ _.
