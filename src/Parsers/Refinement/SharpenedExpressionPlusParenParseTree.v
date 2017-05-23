d9bebee189a04db983509112bcf1fb02a6766dfe
Jason Gross <jgross@mit.edu>
2017-05-23 18:24:50 -0400

    Add action-based evaluator

--- src/Parsers/Refinement/SharpenedExpressionPlusParenParseTree.v
+++ src/Parsers/Refinement/SharpenedExpressionPlusParenParseTree.v
@@ -1,6 +1,7 @@
 (** Sharpened ADT for #, +, () *)
 Require Import Fiat.Parsers.Grammars.ExpressionNumPlusParen.
 Require Import Fiat.Parsers.Refinement.Tactics.
+Require Import Fiat.Parsers.ActionEvaluator.
 Require Import Fiat.Parsers.Refinement.SharpenedExpressionPlusParen.
 Require Import Fiat.Parsers.Grammars.EvalGrammarTactics.
 
@@ -14,6 +15,18 @@ Definition parser_informative (str : Coq.Strings.String.string)
   : option (@simple_parse_of_item Ascii.ascii).
 Proof.
   Time make_parser_informative ComputationalSplitter.
+Defined.
+
+Definition parser_eval (str : Coq.Strings.String.string)
+  : option nat.
+Proof.
+  refine match parser_informative str with
+         | Some (SimpleParseNonTerminal nt pt)
+           => option_map (fun d : digits => d : nat)
+                         (evaluate_item plus_expr_pregrammar_with_actions (SimpleParseNonTerminal nt pt))
+         | Some (SimpleParseTerminal _) => None
+         | None => None
+         end.
 Defined.
 
 Section eval.
@@ -186,7 +199,7 @@ Section evals.
   Defined.
 End evals.
 
-Definition parser_eval (str : Coq.Strings.String.string)
+Definition parser_eval_manual (str : Coq.Strings.String.string)
   : option nat.
 Proof.
   refine match parser_informative str with
@@ -198,6 +211,9 @@ Proof.
          end.
 Defined.
 
+Check let n := 3 in eq_refl (Some n) <: parser_eval_manual "(1+1)+1" = Some n.
+Check let n := 5 in eq_refl (Some n) <: parser_eval_manual "((1+1)+(1+1))+1" = Some n.
+Check let n := 55 in eq_refl (Some n) <: parser_eval_manual "(((((55)))))" = Some n.
 Check let n := 3 in eq_refl (Some n) <: parser_eval "(1+1)+1" = Some n.
 Check let n := 5 in eq_refl (Some n) <: parser_eval "((1+1)+(1+1))+1" = Some n.
 Check let n := 55 in eq_refl (Some n) <: parser_eval "(((((55)))))" = Some n.
