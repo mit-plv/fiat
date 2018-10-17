330dbe17b91a396e32dda81aaf73d73c44ab734a
Clément Pit-Claudel <clement.pitclaudel@live.com>
2018-10-17 16:54:13 -0400

    Step through master_plan to locate the issue with BookStore

--- src/Examples/QueryStructure/Bookstore.v
+++ src/Examples/QueryStructure/Bookstore.v
@@ -112,8 +112,167 @@ Record DelegationADT (Sig : ADTSig)
 Theorem SharpenedBookStore :
   FullySharpened BookStoreSpec.
 Proof.
+  start sharpening ADT.
 
-  master_plan EqIndexTactics.
+  eapply SharpenStep.
+
+  match goal with
+    |- context [@BuildADT (QueryStructure ?Rep) _ _ _ _ _ _] =>
+    eapply refineADT_BuildADT_Rep_refine_All with (AbsR := @DropQSConstraints_AbsR Rep); idtac
+  end.
+
+  exfalso; admit.
+
+  eapply refine_Methods_cons.
+
+  exfalso; admit.
+
+  eapply refine_Methods_cons.
+  exfalso; admit.
+
+  eapply refine_Methods_cons.
+
+  simpl; intros.
+  match goal with
+  | |- refine _ (?E _ _) => let H := fresh in set (H := E)
+  end.
+
+  cbv delta [GetAttribute] beta; simpl.
+
+  remove trivial insertion checks.
+
+  (* The trivial insertion checks involve the fresh id,
+       so we need to drill under the binder before
+       attempting to remove them. *)
+  rewrite refine_bind; set_refine_evar.
+  3: unfold pointwise_relation; intros.
+  3: drop_symmetric_functional_dependencies.
+
+  (* (* 3: fundepToQuery. *) *)
+
+  3: match goal with
+  | |- context [ {b : _ | decides b (forall tup' : IndexedRawTuple, GetUnConstrRelation ?or ?Ridx _ -> @FunctionalDependency_P ?hhint ?attrlist1 ?attrlist2 ?n _)} ]
+    =>
+        let H' := fresh in
+        let H'' := fresh in
+        let refine_fundep := fresh in
+        assert
+         (H' :
+          (forall tup' : IndexedRawTuple, GetUnConstrRelation or Ridx tup' -> @FunctionalDependency_P hhint attrlist1 attrlist2 n (indexedElement tup')) <->
+          (forall tup' : IndexedRawTuple,
+           ~ (GetUnConstrRelation or Ridx tup' /\ tupleAgree n (indexedElement tup') attrlist2 /\ ~ tupleAgree n (indexedElement tup') attrlist1))) by
+         (unfold FunctionalDependency_P; dec_tauto);
+         assert (H'' : DecideableEnsemble (fun x : RawTuple => tupleAgree_computational n x attrlist2 /\ ~ tupleAgree_computational n x attrlist1)) by
+          (subst_all; FunctionalDependencyAutomation.prove_decidability_for_functional_dependencies);
+         (let refine_fundep := eval simpl in (refine_functional_dependency_check_into_query n attrlist2 attrlist1 or H'' H') in
+          setoid_rewrite refine_fundep; clear H'' H')
+     end.
+  3: simplify with monad laws.
+  3: pose_string_hyps.
+
+  Require Import
+        Coq.Strings.String
+        Fiat.Common
+        Fiat.Common.StringBound
+        Fiat.Common.Tactics.CacheStringConstant
+        Fiat.Common.Tactics.HintDbExtra
+        Fiat.Common.Tactics.TransparentAbstract
+        Fiat.Computation.Refinements.General
+        Fiat.Computation.SetoidMorphisms
+        Fiat.QueryStructure.Specification.Representation.Heading
+        Fiat.QueryStructure.Specification.Representation.Schema
+        Fiat.QueryStructure.Specification.Representation.QueryStructureSchema
+        Fiat.QueryStructure.Implementation.DataStructures.BagADT.IndexSearchTerms
+        Fiat.QueryStructure.Implementation.DataStructures.BagADT.QueryStructureImplementation.
+
+  (* (* pose_heading_hyps. *) *)
+  3: (* Commenting this line out makes the proof go through *)
+  repeat foreach [ headingCache ] run (fun id => progress fold id).
+
+  Unshelve.
+  all: exfalso; admit.
+Qed.
+
+
+let FindAttributeUses := EqExpressionAttributeCounter in
+let BuildEarlyIndex := ltac:(LastCombineCase6 BuildEarlyEqualityIndex) in
+let BuildLastIndex := ltac:(LastCombineCase5 BuildLastEqualityIndex) in
+let IndexUse := EqIndexUse in
+let createEarlyTerm := createEarlyEqualityTerm in
+let createLastTerm := createLastEqualityTerm in
+let IndexUse_dep := EqIndexUse_dep in
+let createEarlyTerm_dep := createEarlyEqualityTerm_dep in
+let createLastTerm_dep := createLastEqualityTerm_dep in
+let BuildEarlyBag := BuildEarlyEqualityBag in
+let BuildLastBag := BuildLastEqualityBag in
+let PickIndex := ltac:(fun makeIndex =>
+                           GenerateIndexesForAll FindAttributeUses ltac:(fun attrlist =>
+                                                         let attrlist' := eval compute in (PickIndexes _ (CountAttributes' attrlist)) in makeIndex attrlist')) in
+    PickIndex ltac:(fun attrlist =>
+                      make_simple_indexes attrlist BuildEarlyIndex BuildLastIndex).
+
+compute in (type of H).
+let H := (eval unfold H in H) in
+let t := type of H in
+unify H ((fun _ => False) : t).
+admit.
+
+compute in (type of H).
+let H := (eval unfold H in H) in
+let t := type of H in
+unify H ((fun _ _ _ => False) : t).
+admit.
+
+compute in (type of H).
+let H := (eval unfold H in H) in
+let t := type of H in
+unify H ((fun _ _ _ => False) : t).
+admit.
+
+compute in (type of H).
+let H := (eval unfold H in H) in
+let t := type of H in
+unify H ((fun _ _ _ => False) : t).
+admit.
+
+compute in (type of H).
+let H := (eval unfold H in H) in
+let t := type of H in
+unify H ((fun _ _ _ => False) : t).
+admit.
+
+compute in (type of H).
+let H := (eval unfold H in H) in
+let t := type of H in
+unify H ((fun _ _ _ => False) : t).
+admit.
+
+compute in (type of H).
+let H := (eval unfold H in H) in
+let t := type of H in
+unify H ((fun _ _ _ => False) : t).
+admit.
+
+exfalso.
+admit.
+
+Unshelve.
+admit.
+
+Defined.
+
+let FindAttributeUses := EqExpressionAttributeCounter in
+let BuildEarlyIndex := ltac:(LastCombineCase6 BuildEarlyEqualityIndex) in
+let BuildLastIndex := ltac:(LastCombineCase5 BuildLastEqualityIndex) in
+let IndexUse := EqIndexUse in
+let createEarlyTerm := createEarlyEqualityTerm in
+let createLastTerm := createLastEqualityTerm in
+let IndexUse_dep := EqIndexUse_dep in
+let createEarlyTerm_dep := createEarlyEqualityTerm_dep in
+let createLastTerm_dep := createLastEqualityTerm_dep in
+let BuildEarlyBag := BuildEarlyEqualityBag in
+let BuildLastBag := BuildLastEqualityBag in
+           Implement_Bags BuildEarlyBag BuildLastBag.
 
 Time Defined.
 
