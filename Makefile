mit-plv/fiat

commit 168b8004e1e390d1a1260355c19b33dc6f5d9ef9

Fix $(UPDATE_COQPROJECT_TARGET) so that it uses ML_COMPATIBILITY_FILES rather than ML_COMPATIBILITY_FILES_PATTERN to the @ML4_OR_MLG@ is properly substituted. Also, use "-R Bedrock Bedrock" rather than "-I Bedrock", since -I is used for OCaml path, not for Coq source path.
Use $(UPDATE_COQPROJECT_TARGET) to get proper updated _CoqProject.in
The existing _CoqProject.in had files that are not in the narcissus-release
branch.

Makefile
@@ -311,8 +311,10 @@ install-fiat install-fiat-core install-querystructures install-parsers install-p
 	$(SHOW)'MAKE -f Makefile.coq INSTALL'
 	$(HIDE)$(MAKE) -f Makefile.coq VFILES="$(call vo_to_installv,$(T))" install
 
+# This target is used to update the _CoqProject.in file.
+# Use -R, not -I for Bedrock, as it is Coq input, not OCaml input.
 $(UPDATE_COQPROJECT_TARGET):
-	(echo '-R src Fiat'; echo '-I Bedrock'; echo '-I src/Common/Tactics'; git ls-files "*.v" | grep -v '^$(COMPATIBILITY_FILE)$$' | $(SORT_COQPROJECT); (echo '$(COMPATIBILITY_FILE)'; git ls-files "*.{ml4,mlg}" | $(SORT_COQPROJECT); (echo '$(ML_COMPATIBILITY_FILES_PATTERN)' | tr ' ' '\n'; echo 'src/Common/Tactics/transparent_abstract_plugin.mllib'; echo 'src/Common/Tactics/hint_db_extra_plugin.mllib') | $(SORT_COQPROJECT))) > _CoqProject.in
+	(echo '-R src Fiat'; echo '-R Bedrock Bedrock'; echo '-I src/Common/Tactics'; git ls-files "*.v" | grep -v '^$(COMPATIBILITY_FILE)$$' | $(SORT_COQPROJECT); (echo '$(COMPATIBILITY_FILE)'; git ls-files "*.{ml4,mlg}" | $(SORT_COQPROJECT); (echo '$(ML_COMPATIBILITY_FILES)' | tr ' ' '\n'; echo 'src/Common/Tactics/transparent_abstract_plugin.mllib'; echo 'src/Common/Tactics/hint_db_extra_plugin.mllib') | $(SORT_COQPROJECT))) > _CoqProject.in
 
 ifeq ($(IS_FAST),0)
 # see http://stackoverflow.com/a/9691619/377022 for why we need $(eval $(call ...))

_CoqProject.in
@@ -1,5 +1,5 @@
 -R src Fiat
--I Bedrock
+-R Bedrock Bedrock
 -I src/Common/Tactics
 Bedrock/Memory.v
 Bedrock/Nomega.v
@@ -37,82 +37,6 @@ src/ADTRefinement/Refinements/DelegateMethods.v
 src/ADTRefinement/Refinements/HoneRepresentation.v
 src/ADTRefinement/Refinements/RefineHideADT.v
 src/ADTRefinement/Refinements/SimplifyRep.v
-src/CertifiedExtraction/ADT2CompileUnit.v
-src/CertifiedExtraction/Core.v
-src/CertifiedExtraction/CoreLemmas.v
-src/CertifiedExtraction/ExtendedLemmas.v
-src/CertifiedExtraction/ExtendedPropertiesOfTelescopes.v
-src/CertifiedExtraction/FMapUtils.v
-src/CertifiedExtraction/FacadeLemmas.v
-src/CertifiedExtraction/FacadeNotations.v
-src/CertifiedExtraction/FacadeUtils.v
-src/CertifiedExtraction/FacadeWrappers.v
-src/CertifiedExtraction/HintDBs.v
-src/CertifiedExtraction/PropertiesOfTelescopes.v
-src/CertifiedExtraction/PureFacadeLemmas.v
-src/CertifiedExtraction/PureUtils.v
-src/CertifiedExtraction/RemoveSkips.v
-src/CertifiedExtraction/StringMapUtils.v
-src/CertifiedExtraction/TelAppend.v
-src/CertifiedExtraction/Utils.v
-src/CertifiedExtraction/Benchmarks/Any.v
-src/CertifiedExtraction/Benchmarks/DNS.v
-src/CertifiedExtraction/Benchmarks/MicroEncoders.v
-src/CertifiedExtraction/Benchmarks/MicroEncodersSetup.v
-src/CertifiedExtraction/Benchmarks/MicrobenchmarksAnnotated.v
-src/CertifiedExtraction/Benchmarks/MicrobenchmarksSetup.v
-src/CertifiedExtraction/Benchmarks/ProcessScheduler.v
-src/CertifiedExtraction/Benchmarks/ProcessSchedulerSetup.v
-src/CertifiedExtraction/Benchmarks/results/ProcessSchedulerExtractedMethods.v
-src/CertifiedExtraction/Extraction/Basics.v
-src/CertifiedExtraction/Extraction/BinExpr.v
-src/CertifiedExtraction/Extraction/ConditionalTactics.v
-src/CertifiedExtraction/Extraction/Conditionals.v
-src/CertifiedExtraction/Extraction/Core.v
-src/CertifiedExtraction/Extraction/DeallocSCA.v
-src/CertifiedExtraction/Extraction/Extraction.v
-src/CertifiedExtraction/Extraction/Gensym.v
-src/CertifiedExtraction/Extraction/Internal.v
-src/CertifiedExtraction/Extraction/PreconditionSets.v
-src/CertifiedExtraction/Extraction/BinEncoders/Basics.v
-src/CertifiedExtraction/Extraction/BinEncoders/BinEncoders.v
-src/CertifiedExtraction/Extraction/BinEncoders/Properties.v
-src/CertifiedExtraction/Extraction/BinEncoders/RewriteRules.v
-src/CertifiedExtraction/Extraction/BinEncoders/Wrappers.v
-src/CertifiedExtraction/Extraction/BinEncoders/CallRules/ByteString.v
-src/CertifiedExtraction/Extraction/BinEncoders/CallRules/CallRules.v
-src/CertifiedExtraction/Extraction/BinEncoders/CallRules/Compose.v
-src/CertifiedExtraction/Extraction/BinEncoders/CallRules/DnsCallRules.v
-src/CertifiedExtraction/Extraction/BinEncoders/CallRules/Misc.v
-src/CertifiedExtraction/Extraction/BinEncoders/CallRules/WordLists.v
-src/CertifiedExtraction/Extraction/External/Core.v
-src/CertifiedExtraction/Extraction/External/External.v
-src/CertifiedExtraction/Extraction/External/FacadeADTs.v
-src/CertifiedExtraction/Extraction/External/FacadeLoops.v
-src/CertifiedExtraction/Extraction/External/GenericADTMethods.v
-src/CertifiedExtraction/Extraction/External/GenericMethods.v
-src/CertifiedExtraction/Extraction/External/Lists.v
-src/CertifiedExtraction/Extraction/External/Loops.v
-src/CertifiedExtraction/Extraction/External/ScalarMethods.v
-src/CertifiedExtraction/Extraction/QueryStructures/AllOfLength.v
-src/CertifiedExtraction/Extraction/QueryStructures/Basics.v
-src/CertifiedExtraction/Extraction/QueryStructures/BinNatUtils.v
-src/CertifiedExtraction/Extraction/QueryStructures/Decompose.v
-src/CertifiedExtraction/Extraction/QueryStructures/Deprecated.v
-src/CertifiedExtraction/Extraction/QueryStructures/EnsemblesOfTuplesAndListW.v
-src/CertifiedExtraction/Extraction/QueryStructures/FiatBedrockLemmas.v
-src/CertifiedExtraction/Extraction/QueryStructures/QueryStructures.v
-src/CertifiedExtraction/Extraction/QueryStructures/TupleToListW.v
-src/CertifiedExtraction/Extraction/QueryStructures/Wrappers.v
-src/CertifiedExtraction/Extraction/QueryStructures/WrappersAreConsistent.v
-src/CertifiedExtraction/Extraction/QueryStructures/Zip2.v
-src/CertifiedExtraction/Extraction/QueryStructures/CallRules/CallRules.v
-src/CertifiedExtraction/Extraction/QueryStructures/CallRules/Core.v
-src/CertifiedExtraction/Extraction/QueryStructures/CallRules/Tuple.v
-src/CertifiedExtraction/Extraction/QueryStructures/CallRules/TupleList.v
-src/CertifiedExtraction/Extraction/QueryStructures/CallRules/Tuples0.v
-src/CertifiedExtraction/Extraction/QueryStructures/CallRules/Tuples2.v
-src/CertifiedExtraction/Extraction/QueryStructures/CallRules/WordList.v
 src/Common/BoolFacts.v
 src/Common/BoundedLookup.v
 src/Common/DecideableEnsembles.v

commit 2ba2893dcc3f9139bee30d134abac4067d5b50a8

Fix incorrect change introduced by 168b8004e1e390d1a1260355c19b33dc6f5d9ef9
`_CoqProject.in` should be using `ML_COMPATIBILITY_FILES_PATTERN`, not
`ML_COMPATIBILITY_FILES`, and the compatibility script uses
`EXTRA_PIPE_SED_FOR_COQPROJECT` to update it.

Makefile
@@ -314,7 +314,7 @@ install-fiat install-fiat-core install-querystructures install-parsers install-p
 # This target is used to update the _CoqProject.in file.
 # Use -R, not -I for Bedrock, as it is Coq input, not OCaml input.
 $(UPDATE_COQPROJECT_TARGET):
-	(echo '-R src Fiat'; echo '-R Bedrock Bedrock'; echo '-I src/Common/Tactics'; git ls-files "*.v" | grep -v '^$(COMPATIBILITY_FILE)$$' | $(SORT_COQPROJECT); (echo '$(COMPATIBILITY_FILE)'; git ls-files "*.{ml4,mlg}" | $(SORT_COQPROJECT); (echo '$(ML_COMPATIBILITY_FILES)' | tr ' ' '\n'; echo 'src/Common/Tactics/transparent_abstract_plugin.mllib'; echo 'src/Common/Tactics/hint_db_extra_plugin.mllib') | $(SORT_COQPROJECT))) > _CoqProject.in
+	(echo '-R src Fiat'; echo '-R Bedrock Bedrock'; echo '-I src/Common/Tactics'; git ls-files "*.v" | grep -v '^$(COMPATIBILITY_FILE)$$' | $(SORT_COQPROJECT); (echo '$(COMPATIBILITY_FILE)'; git ls-files "*.{ml4,mlg}" | $(SORT_COQPROJECT); (echo '$(ML_COMPATIBILITY_FILES_PATTERN)' | tr ' ' '\n'; echo 'src/Common/Tactics/transparent_abstract_plugin.mllib'; echo 'src/Common/Tactics/hint_db_extra_plugin.mllib') | $(SORT_COQPROJECT))) > _CoqProject.in
 
 ifeq ($(IS_FAST),0)
 # see http://stackoverflow.com/a/9691619/377022 for why we need $(eval $(call ...))