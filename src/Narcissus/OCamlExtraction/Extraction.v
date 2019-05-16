3600d9c1ac2b5a8aa1a09deeff14cf805892db30
Qianchuan Ye <yeqianchuan@gmail.com>
2019-05-16 16:30:02 -0400

    Fixed extraction.

--- src/Narcissus/OCamlExtraction/Extraction.v
+++ src/Narcissus/OCamlExtraction/Extraction.v
@@ -106,6 +106,7 @@ Extract Inlined Constant ByteBuffer.of_vector => "CstructBytestring.of_vector".
 Extract Inlined Constant ByteBuffer.to_vector => "CstructBytestring.to_vector".
 Extract Inlined Constant ByteBuffer.fold_left => "CstructBytestring.fold_left".
 Extract Inlined Constant ByteBuffer.append => "CstructBytestring.append".
+Extract Inlined Constant ByteBuffer.drop => "CstructBytestring.drop".
 Extract Inlined Constant nth_opt => "CstructBytestring.nth_opt".
 Extract Inlined Constant set_nth' => "CstructBytestring.set_nth".
 Extract Inlined Constant initialize_Aligned_ByteString => "CstructBytestring.create".
