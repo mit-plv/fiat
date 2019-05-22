41c563743fb0806daa1abe950ae51840b3d4b710
Shardul Chiplunkar <shardul.chiplunkar@gmail.com>
2019-05-22 10:04:09 -0400

    Initial commit for firewall code; packet_filters will not compile

---
+++ src/packet_filters.v
@@ -0,0 +1,233 @@
+Require Import Fiat.Narcissus.Examples.NetworkStack.IPv4Header.
+Require Import Fiat.Narcissus.Examples.NetworkStack.TCP_Packet.
+Require Import Bedrock.Word.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Fiat.QueryStructure.Automation.MasterPlan.
+Require Import IndexedEnsembles.
+(*Require Import Fiat.iptables2spec.*)
+
+Definition domain_of (addr: word 32) : word 8 :=
+  (split1 8 24 addr).
+
+Definition domain (d: nat) : word 8 := (natToWord 8 d).
+Definition port (p: nat) : word 16 := (natToWord 16 p).
+
+(* true = fail
+   false = pass *)
+
+Definition fail_if_all (conds: list bool) : bool := (fold_right andb true conds).
+Definition fail_if_any (conds: list bool) : bool := (fold_right orb false conds).
+
+Definition sID := "ID".
+Definition sPACKET := "Packet".
+
+Record Packet := packet
+  { tcp_p: TCP_Packet;
+    ip_h: IPv4_Packet; }.
+
+Definition sHISTORY := "Packet History".
+
+Definition PacketHistorySchema :=
+  Query Structure Schema
+    [ relation sHISTORY has
+              schema < sID :: nat,
+                       sPACKET :: Packet > ]
+      enforcing [].
+
+Require Import Fiat.iptables2spec.
+(* Definition Packet := TupleDef PacketHistorySchema sHISTORY.
+ *)
+Definition FilterSig : ADTSig :=
+    ADTsignature {
+        Constructor "Init" : rep,
+        Method "Filter" : rep * Packet -> rep * bool
+    }.
+
+
+Definition myrule := (-A FORWARD -p tcp -s [ 10 * 0 * 0 * 1 ] --dport 22 -j ACCEPT)%iptables.
+Definition myfirewall :=
+  (
+    *filter
+       myrule
+  )%iptables.
+
+
+Theorem SharpenedMyFirewall:
+  FullySharpened myfirewall.
+Proof.
+  start sharpening ADT.
+
+  Definition StatelessEmpty_AbsR (oldrep: QueryStructure PacketHistorySchema) (newrep: unit) :=
+    True.
+
+(*  hone representation using (@FiniteTables_AbsR PacketHistorySchema). *)
+
+  (*
+  hone representation using (@DropQSConstraints_AbsR PacketHistorySchema).
+  - apply Constructor_DropQSConstraints.
+  - unfold Bind2. simplify with monad laws.
+
+             (UpdateUnConstrRelation qs Ridx
+                (BuildADT.EnsembleInsert tup
+                   (GetUnConstrRelation qs Ridx)), true)
+
+    
+    doAny simplify_queries master_rewrite_drill ltac:(repeat subst_refine_evar; try finish honing).
+    *)
+  hone representation using StatelessEmpty_AbsR.
+  - simplify with monad laws. unfold refine. reflexivity.
+  - Transparent QSInsert. unfold QSInsert. Print freshIdx. simpl. unfold SuccessfulInsertSpec. unfold QSInsertSpec. simpl. intros. computes_to_econstructor.
+
+  
+(* Disallow all cross-domain SSH *)
+(* --> if dst-port == 22 and src-domain != dst-domain, fail, else pass *)
+Definition CrossDomain22FilterSpec : ADT FilterSig :=
+    Eval simpl in Def ADT {
+        rep := QueryStructure PacketHistorySchema,
+        Def Constructor0 "Init" : rep := empty,,
+
+        Def Method1 "Filter" (r: rep) (p: Packet) : rep * bool :=
+            ret (r, (fail_if_all [
+              weqb (port 22) p.(tcp_p).(DestPort) ;
+              negb (weqb (domain_of p.(ip_h).(SourceAddress)) (domain_of p.(ip_h).(DestAddress)))
+            ]))
+    }%methDefParsing.
+
+
+(* Allow FTP transfers from domain 3 to domains 1 and 2, but disallow FTP transfers from 1 and 2 to 3 *)
+(* assuming this means domain 3 cannot initiate any FTP requests in 1 and 2 *)
+(* --> if dst-port == 21 and src-domain == 3 and dst-domain == 1 or 2, fail, else pass *)
+Definition Trusted21FilterSpec : ADT FilterSig :=
+    Eval simpl in Def ADT {
+        rep := QueryStructure PacketHistorySchema,
+        Def Constructor0 "Init" : rep := empty,,
+
+        Def Method1 "Filter" (r: rep) (p: Packet) : rep * bool :=
+            ret (r, (fail_if_all [
+              (weqb (p.(tcp_p).(DestPort)) (port 21)) ;
+              (weqb (domain_of (p.(ip_h).(SourceAddress))) (domain 130)) ;
+              (fail_if_any [
+                (weqb (domain_of (p.(ip_h).(DestAddress))) (domain 110)) ;
+                (weqb (domain_of (p.(ip_h).(DestAddress))) (domain 120))
+              ])
+            ]))
+    }%methDefParsing.
+
+
+Record SimplePacket := 
+  { id: nat }.
+  
+Definition SimpleFilterSig : ADTSig :=
+  ADTsignature {
+      Constructor "Init" : rep,
+      Method "Filter" : rep * SimplePacket -> rep * bool
+  }.
+
+Definition SimplePacketHistorySchema :=
+  Query Structure Schema
+    [ relation sHISTORY has
+              schema < sPACKET :: SimplePacket > ]
+      enforcing [].
+
+
+Locate "!".
+Locate IndexedEnsemble.
+
+Definition isIDHighest (r: QueryStructure SimplePacketHistorySchema) (p: SimplePacket) : Comp bool :=
+(*     vals <- For (pac in r!sHISTORY) Return ((pac!sPACKET).(id)); *)
+    { h: bool | decides h (forall pac, IndexedEnsemble_In ((DropQSConstraints r)!sHISTORY)%QueryImpl pac
+        -> lt (pac!sPACKET).(id) p.(id)) }.
+
+(* rephrase with Ensembles predicate, finiteness not necessary *)
+
+
+Definition IncrementIDFilterSpec : ADT SimpleFilterSig :=
+    Eval simpl in Def ADT {
+        rep := QueryStructure SimplePacketHistorySchema,
+        Def Constructor0 "Init" : rep := empty,,
+
+        Def Method1 "Filter" (r: rep) (p: SimplePacket) : rep * bool :=
+            isHighest <- isIDHighest r p;
+            `(r, _) <- Insert (< sPACKET :: p >) into r!sHISTORY;
+            ret (r, isHighest)
+    }%methDefParsing.
+
+
+Theorem SharpenedIncrementIDFilter:
+  FullySharpened IncrementIDFilterSpec.
+Proof.
+
+  Definition repHighestID (oldrep: QueryStructure SimplePacketHistorySchema) (newrep: option nat) :=
+    match newrep with
+    | Some n =>
+      (forall pac, IndexedEnsemble_In (((DropQSConstraints oldrep)!sHISTORY)%QueryImpl) pac -> le (pac!sPACKET).(id) n)
+      /\ (exists pac, IndexedEnsemble_In (((DropQSConstraints oldrep)!sHISTORY)%QueryImpl) pac /\ (pac!sPACKET).(id) = n)
+    | None => oldrep = QSEmptySpec SimplePacketHistorySchema
+    end.
+  
+  Lemma isIDHighestCompute:
+    forall r_o r_n p, (repHighestID r_o r_n) ->
+      computes_to (isIDHighest r_o p)
+        (match r_n with
+         | Some n => (Nat.ltb n p.(id))
+         | None => true
+         end).
+  Proof.
+    intros. destruct r_n.
+    - eapply PickComputes. unfold decides, If_Then_Else.
+      destruct (n <? id p) eqn:rnpid. 
+      + intros. apply H in H0. apply Nat.ltb_lt in rnpid. intuition.
+      + unfold not. intros. destruct H. apply Nat.ltb_ge in rnpid.
+        assert (Hp: forall pac: RawTuple,
+          IndexedEnsemble_In ((DropQSConstraints r_o)!sHISTORY)%QueryImpl pac ->
+          (lt (id pac!sPACKET) n)). { intros. apply H0 in H2. apply (Nat.lt_le_trans _ _ _ H2 rnpid). }
+        destruct H1 as [pac Hpac]. specialize (Hp pac). destruct Hpac as [HpacIn Hpacn].
+        rewrite Hpacn in Hp. apply Hp, Nat.lt_neq in HpacIn. apply HpacIn. reflexivity.
+    - eapply PickComputes. unfold decides, If_Then_Else. unfold repHighestID in H. subst.
+      intros. unfold QSEmptySpec in H. compute in H. destruct H. inversion H.
+  Qed.
+
+  Lemma isIDHighestRefine:
+    forall r_o r_n p, (repHighestID r_o r_n) ->
+      refine (isIDHighest r_o p)
+        (ret match r_n with
+            | Some n => (Nat.ltb n p.(id))
+            | None => true
+            end).
+  Proof.
+    intros. unfold refine in *. intros. computes_to_inv. subst. apply isIDHighestCompute, H.
+  Qed.
+
+  start sharpening ADT.
+  hone representation using (@DropQSConstraints_AbsR SimplePacketHistorySchema).
+  - simplify with monad laws. unfold refine. intros. computes_to_econstructor.
+    hone representation using repHighestID; unfold repHighestID in *.
+  - simplify with monad laws. refine pick val (@None nat). subst H. reflexivity. reflexivity.
+  - simplify with monad laws. rewrite isIDHighestRefine. simplify with monad laws.
+    unfold refine. intros. computes_to_econstructor. computes_to_econstructor.
+
+
+Definition SYNFloodFilterSpec : ADT FilterSig :=
+    Eval simpl in Def ADT {
+        rep := QueryStructure PacketHistorySchema,
+        Def Constructor0 "Init" : rep := empty,,
+
+        Def Method1 "Filter" (r: rep) (p: Packet) : rep * bool :=
+            src_addr <- ret p.(ip_h)!SourceAddress;
+            dst_addr <- ret p.(ip_h)!DestAddress;
+            src_port <- ret p.(tcp_p)!SourcePort;
+            dst_port <- ret p.(tcp_p)!DestPort;
+            conns <- Count (For (pac in r!sHISTORY)
+                            Where (src_addr = pac.(ip_h)!SourceAddress)
+                            Where (dst_addr = pac.(ip_h)!DestAddress)
+                            Where (dst_port = pac.(tcp_p)!DestPort)
+                            Return ());
+    }%methDefParsing.
+
+
+(* spec a filter that ensures every packet has a higher id than previous
+   specify concretely how we are ensuring this: get/put +1 *)
+(* spec a filter for example #3 from email *)
+(* break down master_plan and try to sharpen filter -- write a tactic, read master_plan *)
