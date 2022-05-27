92b6bb4774476b0a27f8551cc67a7233a78c3b61
Rob Dickerson <rc.dickerson@gmail.com>
2022-05-27 14:15:33 -0400

    Adding unshelve to maybe tactic to discharge goals even in presence of evars.

--- src/Narcissus/Automation/Error.v
+++ src/Narcissus/Automation/Error.v
@@ -4,7 +4,7 @@ Require Import
 
 Inductive Maybe (A : Type) :=
 | Failure (s : string)
-| Success (p : Type).
+| Success (a : Type).
 
 Arguments Failure {A} _.
 Arguments Success {A} _.
@@ -20,10 +20,11 @@ Ltac no_error :=
 Ltac throw s :=
   match goal with
     H := ErrorMessage ?s' |- _ => first [ is_evar s'; unify s s'
-                                      | idtac ] (* TODO: Something better when there is already an error? *)
+                                      (* TODO: Something better when there is already an error? *)
+                                      | idtac ]
   end.
 
-Axiom failing_case : forall (A: Type), A.
+Axiom failing_case : forall (e: ErrorReport) (A: Type), A.
 
 Ltac maybe tac errorMsg :=
   match goal with
@@ -33,12 +34,23 @@ Ltac maybe tac errorMsg :=
   lazymatch goal with
   | |- Maybe ?A =>
       let H := fresh in
-      assert A as H by (ltac:(tac ());
+      assert A as H by (unshelve ltac:(tac ());
                        (* If we make it here, the tactic failed to discharge all goals. *)
-                       throw errorMsg; apply failing_case) ;
+                       throw errorMsg; apply failing_case; exact (ErrorMessage errorMsg)) ;
       first [ no_error; exact (Success H)
             | match goal with
                 M := ErrorMessage ?s' |- _ => exact (Failure s')
               end ]
   | _ => fail "Expected a goal with type Maybe."
   end.
+
+
+(* Example *)
+
+(* Ltac foo := intros; auto. *)
+(* Ltac bar := ltac:(foo). *)
+
+(* Lemma baz : Maybe (forall n : nat, n = n). *)
+(*   maybe ltac:(fun _ => bar) "error in baz"%string. *)
+(*   Show Proof. *)
+(* Defined. *)
